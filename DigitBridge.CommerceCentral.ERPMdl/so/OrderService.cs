bdd039d5b2372381e9b75fc4440868ff5dd20c16
junxian <junxian.cui@hurraymart.com>
2021-07-02 15:52:20 +0800

    add order service and past test

---
+++ DigitBridge.CommerceCentral.ERPMdl/so/OrderService.cs
@@ -0,0 +1,22 @@
+
+    
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using DigitBridge.Base.Utility;
+using DigitBridge.CommerceCentral.YoPoco;
+using DigitBridge.CommerceCentral.ERPDb;
+
+namespace DigitBridge.CommerceCentral.ERPMdl
+{
+    public partial class OrderService
+    {
+
+    }
+}
+
+
+
