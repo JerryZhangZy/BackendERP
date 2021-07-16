f5c4194b607a2345edeee0819fbf22085883c401
wenjian <jian.wen@hurraymart.com>
2021-07-16 18:33:00 +0800

    Add InventoryLogApi

---
+++ DigitBridge.CommerceCentral.ERPApi/MyAppHelper.cs
@@ -0,0 +1,15 @@
+﻿using DigitBridge.CommerceCentral.YoPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.ERPApi
+{
+    public class MyAppHelper
+    {
+        public static DataBaseFactory GetDatabase()
+        {
+            return new DataBaseFactory("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DigitBridgeERP;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
