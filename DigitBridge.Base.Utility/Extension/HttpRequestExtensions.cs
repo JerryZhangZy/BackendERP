f4671dd12e2e200e647982f28942aab8df302002
junxian <junxian.cui@hurraymart.com>
2021-07-16 13:29:42 +0800

    add HttpRequest extensions add config helper add get order  api

---
+++ DigitBridge.Base.Utility/Extension/HttpRequestExtensions.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.Base.Utility
+{
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Get nullable value type data from route datas
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="req"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T? GetRouteData<T>(this HttpRequest req, string key) where T : struct
+        {
+            if (req.HttpContext.GetRouteData().Values.ContainsKey(key))
+            {
+                var value = req.HttpContext.GetRouteData().Values[key].ToString();
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            return null;
+        }
+        /// <summary>
+        /// Get nullable reference type data from route datas
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="req"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetRouteObject<T>(this HttpRequest req, string key) where T : class
+        {
+            if (req.HttpContext.GetRouteData().Values.ContainsKey(key))
+            {
+                var value = req.HttpContext.GetRouteData().Values[key].ToString();
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// Get nullable value type data from context headers
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="req"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T? GetHeaderData<T>(this HttpRequest req, string key) where T : struct
+        {
+            if (req.Headers.ContainsKey(key))
+            {
+                var value = req.Headers[key].ToString();
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get nullable reference type data from headers
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="req"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetHeaderObject<T>(this HttpRequest req, string key) where T : class
+        {
+            if (req.Headers.ContainsKey(key))
+            {
+                var value = req.Headers[key].ToString();
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            return null;
+        }
+    }
+}
--- DigitBridge.CommerceCentral.ERPApi/SalesOrderApi.cs
+++ DigitBridge.CommerceCentral.ERPApi/SalesOrderApi.cs
@@ -1,7 +1,10 @@
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.ERPDb;
+using DigitBridge.CommerceCentral.ERPMdl;
+using DigitBridge.CommerceCentral.YoPoco;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -10,40 +13,40 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
+using DigitBridge.Base.Utility;
 
 namespace DigitBridge.CommerceCentral.ERPApi
 {
+    /// <summary>
+    /// Process salesorder
+    /// </summary> 
     public static class SalesOrderApi
     {
-        [FunctionName(nameof(GetSalesOrders))]
+        /// <summary>
+        /// Get salesorder
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
         [OpenApiOperation(operationId: "GetSalesOrders", tags: new[] { "SalesOrders" })]
-        [OpenApiParameter(name: "masterAccountNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "MasterAccountNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "profileNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "$top", In = ParameterLocation.Query, Required = false, Type = typeof(int), Summary = "$top", Description = "Page size. Default value is 100. Maximum value is 100.", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "$skip", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = "$skip", Description = "Records to skip. https://github.com/microsoft/api-guidelines/blob/vNext/Guidelines.md", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "$count", In = ParameterLocation.Query, Required = false, Type = typeof(bool), Summary = "$count", Description = "Valid value: true, false. When $count is true, return total count of records, otherwise return requested number of data.", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "$sortBy", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = "$sortBy", Description = "sort by. Default order by LastUpdateDate. ", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "orderNumber", In = ParameterLocation.Path, Required = false, Type = typeof(string), Summary = "orderNumber", Description = "Sales Order Number. ", Visibility = OpenApiVisibilityType.Advanced)]
+        [FunctionName("GetSalesOrders")]
+        [OpenApiParameter(name: "orderNumber", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "orderNumber", Description = "Sales Order Number. ", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SalesOrderDataDto), Example = typeof(SalesOrderDataDto), Description = "The OK response")]
         public static async Task<IActionResult> GetSalesOrders(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "salesOrders/{orderNumber?}")] HttpRequest req,
-            string orderNumber,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "salesOrders/{orderNumber}")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var orderNumber = req.GetRouteObject<string>("orderNumber");
+            var dataBaseFactory = new DataBaseFactory(ConfigHelper.Dsn);
+            var srv = new SalesOrderService(dataBaseFactory);
+            var success = await srv.GetByOrderNumberAsync(orderNumber);
+            if (success)
+            {
+                var dto = srv.ToDto(srv.Data);
+                return new OkObjectResult(dto);
+            }
+            else
+                return new NoContentResult();
         }
     }
 }
