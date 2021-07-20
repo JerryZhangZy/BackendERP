024a22d0d9203908a8d6aaf4d7d65749777a4040
wenjian <jian.wen@hurraymart.com>
2021-07-20 12:18:06 +0800

    base on develop and add inventoryapi sevices&functions with test

--- DigitBridge.CommerceCentral.ERPMdl/inventoryLog/InventoryLogService.cs
+++ DigitBridge.CommerceCentral.ERPMdl/inventoryLog/InventoryLogService.cs
@@ -11,22 +11,113 @@ using System.Threading.Tasks;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.YoPoco;
 using DigitBridge.CommerceCentral.ERPDb;
+using Microsoft.Data.SqlClient;
 
 namespace DigitBridge.CommerceCentral.ERPMdl
 {
     public partial class InventoryLogService
     {
-
+        private Inventory inventory;
         /// <summary>
         /// Initiate service objcet, set instance of DtoMapper, Calculator and Validator 
         /// </summary>
         public override InventoryLogService Init()
         {
             base.Init();
+            inventory = new Inventory(dbFactory);
             SetDtoMapper(new InventoryLogDataDtoMapperDefault());
             SetCalculator(new InventoryLogServiceCalculatorDefault());
             AddValidator(new InventoryLogServiceValidatorDefault());
             return this;
+        }
+
+        private InventoryLogDataDtoMapperDefault _mapper = new InventoryLogDataDtoMapperDefault();
+
+        private InventoryData GetInventoryById(string productUuid)
+        {
+            var data = new InventoryData(dbFactory);
+            if (data.GetById(productUuid))
+                return data;
+            return null;
+        }
+
+        public long GetBatchNum()
+        {
+            return dbFactory.Db.ExecuteScalar<long>("select max(BatchNum) as maxBatchNum from InventoryLog;") + 1;
+        }
+
+        public string FindProductUuidBySku(int masterAccountNum, int profileNum, string sku)
+        {
+            return dbFactory.Db.FirstOrDefault<string>($"select ProductUuid from Inventory where Sku='{sku}' and MasterAccountNum = {masterAccountNum} and ProfileNum={profileNum}");
+        }
+
+        public InventoryData GetInventoryBySku(int masterAccountNum, int profileNum, string sku)
+        {
+            var productUuid = FindProductUuidBySku(masterAccountNum, profileNum, sku);
+            return GetInventoryById(productUuid);
+        }
+        public int DeleteByLogUuid(string logUuid)
+        {
+            var list = InventoryLogHelper.QueryInventoryLogByUuid(logUuid);
+            return list.SetDataBaseFactory(dbFactory).Delete();
+        }
+
+        public List<InventoryLogDto> GetListByUuid(string logUuid)
+        {
+            var list = InventoryLogHelper.QueryInventoryLogByUuid(logUuid);
+            return _mapper.WriteInventoryLogDtoList(list, null);
+        }
+
+        public int AddList(List<InventoryLogDto> dtoList)
+        {
+            if (dtoList == null || dtoList.Count() == 0)
+                return 0;
+            if (dtoList.Exists(r => string.IsNullOrEmpty(r.SKU)))
+                return 0;
+            if (dtoList.Exists(r => string.IsNullOrEmpty(r.LogUuid)))
+                return 0;
+            var svc = new InventoryData(dbFactory);
+            var batchNum = GetBatchNum();
+
+            dtoList.ForEach(x =>
+            {
+                x.BatchNum = batchNum;
+                x.RowNum = 0;
+                x.InventoryLogUuid = Guid.NewGuid().ToString();
+                var inventoryData = GetInventoryBySku(x.MasterAccountNum.Value, x.ProfileNum.Value, x.SKU);//svc.GetInventoryBySku(x.DatabaseNum.Value, x.MasterAccountNum.Value, x.ProfileNum.Value, x.SKU);
+                if (inventoryData != null)
+                {
+                    var inventory = inventoryData.Inventory.First();
+                    x.ProductUuid = inventory.ProductUuid;
+                    x.InventoryUuid = inventory.InventoryUuid;
+                    x.WarehouseCode = inventory.WarehouseCode;
+                    x.BeforeInstock = inventory.Instock;
+                    x.BeforeBaseCost = inventory.BaseCost;
+                    x.BeforeUnitCost = inventory.UnitCost;
+                    x.BeforeAvgCost = inventory.AvgCost;
+                }
+            });
+            var mapper = new InventoryLogDataDtoMapperDefault();
+            var datalist = mapper.ReadInventoryLogDtoList(null, dtoList);
+            var addcount = datalist.Count();
+            return datalist.SetDataBaseFactory(dbFactory).Save() ? addcount : 0;
+            //TODO:Validator验证，Mapper,补充完整Inventory信息进去，调用Dao
+        }
+
+        public int UpdateInventoryLogList(List<InventoryLogDto> list)
+        {
+            if (list == null || list.Count() == 0)
+                return 0;
+            var logUuidList = list.Select(r => r.LogUuid).Distinct();
+
+            var dlist = new List<InventoryLog>();
+            foreach (var uuid in logUuidList)
+            {
+                dlist.AddRange(InventoryLogHelper.QueryInventoryLogByUuid(uuid));
+            }
+            var data = _mapper.ReadInventoryLogDtoList(dlist, list).Where(x => x.RowNum > 0).ToList();
+            int result = data.Count();
+            return data.SetDataBaseFactory(dbFactory).Save() ? result : 0;
         }
 
 
