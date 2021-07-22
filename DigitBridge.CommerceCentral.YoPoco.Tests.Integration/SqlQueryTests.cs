3caa1ab1ca45d4c49518a956a0aef3cdea5d2591
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2021-07-22 10:00:28 -0700

    Upgrade to use Managed Identity.

--- DigitBridge.CommerceCentral.YoPoco.Tests.Integration/SqlQueryTests.cs
+++ DigitBridge.CommerceCentral.YoPoco.Tests.Integration/SqlQueryTests.cs
@@ -81,6 +81,9 @@ namespace DigitBridge.CommerceCentral.ERPMdl.Tests.Integration
             }
             catch (Exception ex)
             {
+                //Cannot open server 'bobotestsql' requested by the login. Client with IP address '174.81.9.150' is not allowed to access the server.
+                //To enable access, use the Windows Azure Management Portal or run sp_set_firewall_rule on the master database to create a firewall rule
+                //for this IP address or address range.  It may take up to five minutes for this change to take effect.
                 throw;
             }
 
