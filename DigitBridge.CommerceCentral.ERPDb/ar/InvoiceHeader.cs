d517551f47199368ed1b680e3a85a7cb5bce34b5
junxian <junxian.cui@hurraymart.com>
2021-07-28 09:17:42 +0800

    invoicepayemnt get api return both invoiceheader and invoicetrancation

--- DigitBridge.CommerceCentral.ERPDb/ar/InvoiceHeader.cs
+++ DigitBridge.CommerceCentral.ERPDb/ar/InvoiceHeader.cs
@@ -1,11 +1,12 @@
 
-              
-    
+
+
 
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.YoPoco;
@@ -14,7 +15,15 @@ namespace DigitBridge.CommerceCentral.ERPDb
 {
     public partial class InvoiceHeader
     {
-
+        /// <summary>
+        /// get InvoiceHeader by invoiceNumber
+        /// </summary>
+        /// <param name="invoiceNumber"></param>
+        /// <returns></returns>
+        public virtual async Task<InvoiceHeader> GetByInvoiceNumberAsync(string invoiceNumber)
+        {
+            return (await dbFactory.FindAsync<InvoiceHeader>($"SELECT TOP 1 * FROM InvoiceHeader where InvoiceNumber='{invoiceNumber}'")).FirstOrDefault();
+        }
     }
 }
 
