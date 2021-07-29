08e7c4679a4dedf3a7c271b3747c8ab8c9bc5618
junxian <junxian.cui@hurraymart.com>
2021-07-29 15:17:57 +0800

    Write unhandle exception to sentry and seri Add DigitBridge.Log project

---
+++ DigitBridge.Log/Log_Console.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace DigitBridge.Log
+{
+    internal class Log_Console : LogBase
+    { 
+        private Log_Console():base(SeriLogInstance.GetConsoleLogger())
+        {
+             
+        }
+
+        #region Instance
+
+        private static object _lockObj = new object();
+        private static Log_Console _instance;
+
+        public static Log_Console Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Log_Console();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        #endregion
+    }
+}
