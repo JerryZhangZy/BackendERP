27764b72d31d8207ad88f9b06b1cfdea0ddcb5a9
liyunman <35281214+liyunman@users.noreply.github.com>
2021-07-28 20:55:45 -0700

    add sql

--- DigitBridge.CommerceCentral.ERPDatabase/channelOrder/OrderLine_Update.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/channelOrder/OrderLine_Update.sql
@@ -1,5 +1,16 @@
 ﻿
 -- 07/28/20201 By Jerry Z 
+
+IF COL_LENGTH('OrderLine', 'CentralOrderUuid') IS NULL					
+BEGIN					
+    ALTER TABLE OrderLine ADD [CentralOrderUuid] VARCHAR(50) NOT NULL DEFAULT ''
+    CREATE NONCLUSTERED INDEX [FK_OrderLine_CentralOrderUuid] ON [dbo].[OrderLine]
+    (
+	    [CentralOrderUuid] ASC,
+	    [CentralOrderLineNum] ASC
+    );
+END	
+
 IF COL_LENGTH('OrderLine', 'CentralOrderLineUuid') IS NULL					
 BEGIN					
     ALTER TABLE OrderLine ADD [CentralOrderLineUuid] VARCHAR(50) NOT NULL DEFAULT (CAST(newid() AS NVARCHAR(50)))
@@ -13,16 +24,7 @@ IF COL_LENGTH('OrderLine', 'RowNum') IS NULL
 BEGIN					
     ALTER TABLE OrderLine ADD [RowNum] BIGINT NOT NULL DEFAULT 0
 END					
-
-IF COL_LENGTH('OrderLine', 'CentralOrderUuid') IS NULL					
-BEGIN					
-    ALTER TABLE OrderLine ADD [CentralOrderUuid] VARCHAR(50) NOT NULL DEFAULT ''
-    CREATE NONCLUSTERED INDEX [FK_OrderLine_CentralOrderUuid] ON [dbo].[OrderLine]
-    (
-	    [CentralOrderUuid] ASC,
-	    [CentralOrderLineNum] ASC
-    );
-END					
+				
 
 /*
     UPDATE spp
