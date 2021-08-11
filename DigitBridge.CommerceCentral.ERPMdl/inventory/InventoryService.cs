8e944ec53909674ca2f6c2271c9f936e7a478ba1
wenjian <jian.wen@hurraymart.com>
2021-08-11 16:07:33 +0800

    fixed productex api;use inventorypayload instead productexpayload; remove productexpayload;

--- DigitBridge.CommerceCentral.ERPMdl/inventory/InventoryService.cs
+++ DigitBridge.CommerceCentral.ERPMdl/inventory/InventoryService.cs
@@ -221,6 +221,8 @@ namespace DigitBridge.CommerceCentral.ERPMdl
             if (!(await ValidateAccountAsync(payload).ConfigureAwait(false)))
                 return false;
 
+            Edit();
+
             if (!(await ValidateAsync(payload.Inventory).ConfigureAwait(false)))
                 return false;
 
@@ -236,29 +238,8 @@ namespace DigitBridge.CommerceCentral.ERPMdl
 
             return await SaveDataAsync();
         }
-        public async Task<bool> AddAsync(ProductExPayload payload)
-        {
-            if (payload is null || !payload.HasInventoryData)
-                return false;
 
-            // set Add mode and clear data
-            Add();
-            if (!(await ValidateAsync(payload.InventoryData).ConfigureAwait(false)))
-                return false;
-            // load data from dto
-            FromDto(payload.InventoryData);
-            //TODO merge
-            //if (!(await ValidatePayloadAsync(payload).ConfigureAwait(false)))
-            //    return false;
-
-            // validate data for Add processing
-            if (!(await ValidateAsync().ConfigureAwait(false)))
-                return false;
-
-            return await SaveDataAsync().ConfigureAwait(false);
-        }
-
-        public async Task<bool> DeleteBySkuAsync(ProductExPayload payload,string sku)
+        public async Task<bool> DeleteBySkuAsync(InventoryPayload payload,string sku)
         {
             if (string.IsNullOrEmpty(sku))
                 return false;
@@ -274,32 +255,8 @@ namespace DigitBridge.CommerceCentral.ERPMdl
             return success && (await DeleteDataAsync());
         }
 
-        public async Task<bool> UpdateAsync(ProductExPayload payload)
-        {
 
-            if (payload is null || !payload.HasInventoryData || payload.InventoryData.ProductBasic.RowNum.ToLong() <= 0)
-                return false;
-            // set Add mode and clear data
-            await EditAsync(payload.InventoryData.ProductBasic.RowNum.ToLong()).ConfigureAwait(false);
-
-            if (!(await ValidateAsync(payload.InventoryData).ConfigureAwait(false)))
-                return false;
-
-            // validate data for Add processing
-            //TODO merge
-            //if (!(await ValidatePayloadAsync(payload).ConfigureAwait(false)))
-            //    return false;
-
-            // load data from dto
-            FromDto(payload.InventoryData);
-            // validate data for Add processing
-            if (!(await ValidateAsync().ConfigureAwait(false)))
-                return false;
-
-            return await SaveDataAsync();
-        }
-
-        public async Task<ProductExPayload> GetInventoryBySkuArrayAsync(ProductExPayload payload)
+        public async Task<InventoryPayload> GetInventoryBySkuArrayAsync(InventoryPayload payload)
         {
             if (!payload.HasSkus)
                 return payload;
@@ -312,12 +269,12 @@ namespace DigitBridge.CommerceCentral.ERPMdl
                 else
                     msglist.AddError($"ProductSku:{code} no found");
             }
-            payload.InventoryDatas = list;
+            payload.Inventorys = list;
             payload.Messages = msglist;
             return payload;
         }
 
-        public async Task<bool> GetInventoryBySkuAsync(ProductExPayload payload, string sku)
+        public async Task<bool> GetInventoryBySkuAsync(InventoryPayload payload, string sku)
         {
             if (string.IsNullOrEmpty(sku))
                 return false;
