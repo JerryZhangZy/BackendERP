ec6d87c0dc5c9b20c2123bf497eb2fc8041d758f
liyunman <35281214+liyunman@users.noreply.github.com>
2021-08-18 10:58:49 -0700

    yunman

---
+++ DigitBridge.CommerceCentral.ERPMdl/Manager/CreateSalesOrderDto.cs
@@ -0,0 +1,24 @@
+﻿using DigitBridge.CommerceCentral.ERPDb;
+using DigitBridge.CommerceCentral.ERPMdl.Base;
+using DigitBridge.CommerceCentral.YoPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace DigitBridge.CommerceCentral.ERPMdl.Manager
+{
+    [Serializable()]
+    public class CreateSalesOrderDto
+    {
+        public ChannelOrderDataDto ChannelOrder { get; set; }
+        [JsonIgnore, XmlIgnore, IgnoreCompare]
+        public bool HasChannelOrder => ChannelOrder != null;
+
+        public SalesOrderDataDto SalesOrder { get; set; }
+        [JsonIgnore, XmlIgnore, IgnoreCompare]
+        public bool HasSalesOrder => SalesOrder != null;
+    }
+
+}
---
+++ DigitBridge.CommerceCentral.ERPMdl/Manager/SalesOrderMessage.cs
@@ -0,0 +1,21 @@
+﻿using DigitBridge.CommerceCentral.ERPDb;
+using DigitBridge.CommerceCentral.YoPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace DigitBridge.CommerceCentral.ERPMdl.Manager
+{
+    [Serializable()]
+    public class SalesOrderMessage 
+    {
+        public void GetChannelOrder(string uuid)
+        {
+            
+            var srv = new ChannelOrderService()
+        }
+    }
+
+}
