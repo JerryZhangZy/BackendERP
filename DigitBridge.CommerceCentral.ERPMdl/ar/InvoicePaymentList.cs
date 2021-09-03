382543400111ee57fccae77afc1a1e40588b2b03
wenjian <jian.wen@hurraymart.com>
2021-09-03 10:33:43 +0800

    fixed payment,return import&export;

--- DigitBridge.CommerceCentral.ERPMdl/ar/InvoicePaymentList.cs
+++ DigitBridge.CommerceCentral.ERPMdl/ar/InvoicePaymentList.cs
@@ -100,7 +100,70 @@ SELECT
                 throw;
             }
             return payload;
-        } 
+        }
+
+        public virtual async Task<IList<long>> GetRowNumListAsync(InvoicePaymentPayload payload)
+        {
+            if (payload == null)
+                payload = new InvoicePaymentPayload();
+
+            this.LoadRequestParameter(payload);
+            var rowNumList = new List<long>();
+
+            var sql = $@"
+SELECT distinct {Helper.TableAllies}.RowNum 
+{GetSQL_from()} 
+{GetSQL_where()}
+ORDER BY  {Helper.TableAllies}.RowNum  
+OFFSET {payload.FixedSkip} ROWS FETCH NEXT {payload.FixedTop} ROWS ONLY
+";
+            try
+            {
+                using var trs = new ScopedTransaction(dbFactory);
+                rowNumList = await SqlQuery.ExecuteAsync(
+                    sql,
+                    (long rowNum) => rowNum,
+                    GetSqlParameters().ToArray()
+                );
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return rowNumList;
+        }
+
+        public virtual IList<long> GetRowNumList(InvoicePaymentPayload payload)
+        {
+            if (payload == null)
+                payload = new InvoicePaymentPayload();
+
+            this.LoadRequestParameter(payload);
+            var rowNumList = new List<long>();
+            var sql = $@"
+SELECT distinct {Helper.TableAllies}.RowNum 
+{GetSQL_from()} 
+{GetSQL_where()}
+ORDER BY  {Helper.TableAllies}.RowNum  
+OFFSET {payload.FixedSkip} ROWS FETCH NEXT {payload.FixedTop} ROWS ONLY
+";
+            try
+            {
+                using var trs = new ScopedTransaction(dbFactory);
+                rowNumList = SqlQuery.Execute(
+                    sql,
+                    (long rowNum) => rowNum,
+                    GetSqlParameters().ToArray()
+                );
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return rowNumList;
+        }
+
+
         //TODO where sql&more column displayed.
         private string GetExportSql()
         { 
