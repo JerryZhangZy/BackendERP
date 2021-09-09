036bd1556ed4e93a4a02a69f688d04672838d1c6
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2021-09-09 10:08:56 -0700

    Add T4 template to generate JS files for front end.

---
+++ DigitBridge.CommerceCentral.ERPApi/JsType/so/store/index.ts
@@ -0,0 +1,10 @@
+export type { 
+    SalesOrderDataModel,
+    salesOrderDataInit,
+ } from "./salesOrderDataModel";
+
+export { 
+    getStore, 
+    useCurrentStore as useSalesOrderDataStore, 
+} from "./salesOrderDataStore";
+
