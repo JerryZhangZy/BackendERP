e8e771e296c1bc5d6f28646b68ce9e3447a79ae6
junxian <junxian.cui@hurraymart.com>
2021-09-23 15:43:28 +0800

    Add invoice mapper

---
+++ DigitBridge.QuickBooks.Integration/Model/Enum/QboEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.QuickBooks.Integration
+{
+    public enum SaleOrderQboType
+    {
+        Null = -1,
+        Invoice = 0,
+        SalesReceipt = 1,
+        DailySummarySalesReceipt = 2,
+        DailySummaryInvoice = 3,
+        DoNotExport = 4
+    }
+}
