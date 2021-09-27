0da11bfc4bf187d3ced6d9c9025073e08f008e78
wenjian <jian.wen@hurraymart.com>
2021-09-27 19:04:44 +0800

    fixed complied error

--- DigitBridge.QuickBooks.Integration/Model/AppConstants.cs
+++ DigitBridge.QuickBooks.Integration/Model/AppConstants.cs
@@ -79,7 +79,7 @@ namespace DigitBridge.QuickBooks.Integration
     {
         public const string PaymentTypeExpense = "Expense";//--Payment is reimbursement for expense paid by cash made on behalf of the customer
         //Check,//--Payment is reimbursement for expense paid by check made on behalf of the customer
-        public const string PaymentTypeCreditCardCredit = "CreditCardCredit",//--Payment is reimbursement for a credit card credit made on behalf of the customer
+        public const string PaymentTypeCreditCardCredit = "CreditCardCredit";//--Payment is reimbursement for a credit card credit made on behalf of the customer
             //JournalEntry,//--Payment is linked to the representative journal entry
             //CreditMemo,//--Payment is linked to the credit memo the customer has with the business
             //Invoice,//--The invoice to which payment is applied 
