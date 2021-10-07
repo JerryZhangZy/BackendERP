0437435e7fec7681b8eac70a0762716cc59685f8
wenjian <jian.wen@hurraymart.com>
2021-10-07 22:25:17 +0800

    add AzureTableUniversal and tester

---
+++ DigitBridge.CommerceCentral.AzureStorage/AzureTableUniversal.cs
@@ -0,0 +1,46 @@
+﻿using DigitBridge.Blob;
+using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitBridge.CommerceCentral.AzureStorage
+{
+    public class AzureTableUniversal<T> where T: TableEntity
+    {
+        protected MsAzureTableOperator tableOperator;
+
+        private AzureTableUniversal(MsAzureTableOperator azureTableOperator)
+        {
+            tableOperator = azureTableOperator;
+        }
+
+        public static async Task<AzureTableUniversal<T>> CreateAsync(string tableName, string storageAccountKey, bool createIfNotExists = true)
+        {
+            var tableOperator = await MsAzureTableOperator.CreateTableAsync(tableName, storageAccountKey, createIfNotExists);
+            return new AzureTableUniversal<T>(tableOperator);
+        }
+
+        public async Task<T> AddOrUpdateAsync(T entity) => (T)await tableOperator.InsertOrMergeEntityAsync<T>(entity);
+
+        public async Task DeleteAsync(TableEntity entity)
+        {
+            var delete = TableOperation.Delete(entity);
+            await tableOperator._cloudTable.ExecuteAsync(delete);
+        }
+
+        public async Task<TableEntity> GetByRowKey(string rowKey)
+        {
+            var query = new TableQuery<TableEntity>().Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, rowKey));
+            return tableOperator._cloudTable.ExecuteQuery(query).FirstOrDefault();
+        }
+
+        //public async Task<IList<TableEntity>> QueryAsync(string key,string value)
+        //{
+        //    var query = new TableQuery<TableEntity>().Where($"{key} eq '{value}'");
+        //    return tableOperator._cloudTable.ExecuteQuery(query).ToList();
+        //}
+    }
+}
---
+++ DigitBridge.CommerceCentral.AzureStorage/TableUniversal.cs
@@ -0,0 +1,70 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Azure.Data.Tables.Models;
+using DigitBridge.CommerceCentral.YoPoco;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitBridge.CommerceCentral.AzureStorage
+{
+    public class TableUniversal
+    {
+        protected TableClient tableClient;
+
+        private TableUniversal(TableClient client)
+        {
+            tableClient = client;
+        }
+
+        public static async Task<TableUniversal> CreateAsync(string tableName, string connectionString, bool createIfNotExists = true)
+        {
+            var client = new TableClient(connectionString, tableName);
+            if (createIfNotExists)
+                await client.CreateIfNotExistsAsync();
+            return new TableUniversal(client);
+        }
+
+        public async Task<Response> AddEntityAsync(TableEntity entity)
+        {
+            return await tableClient.AddEntityAsync(entity);
+        }
+
+        public async Task<Response> UpSertEntityAsync(TableEntity entity)
+        {
+            var source =await GetEntityAsync(entity.RowKey, entity.PartitionKey);
+            return await tableClient.UpsertEntityAsync(entity);
+        }
+
+        public async Task<Response> UpdateEntityAsync(TableEntity entity)
+        {
+            var source =await GetEntityAsync(entity.RowKey, entity.PartitionKey);
+            return await tableClient.UpdateEntityAsync(entity,source.ETag);
+        }
+
+        public async Task<TableEntity> GetEntityAsync(string rowKey , string partitionKey )
+        {
+            try
+            {
+                return await tableClient.GetEntityAsync<TableEntity>(partitionKey, rowKey);
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Response> DeleteEntityAsync(string rowKey,string partitionKey)
+        {
+            return await tableClient.DeleteEntityAsync(partitionKey, rowKey);
+        }
+
+        public Pageable<TableEntity> QueryEntityAsync(string key,string value)
+        {
+            return tableClient.Query<TableEntity>($"{key} eq '{value}'");
+        }
+    }
+}
