cd016d3c44d753918f955a7ff4ce3011aae054be
wenjian <jian.wen@hurraymart.com>
2021-10-08 21:35:06 +0800

    Add QueueUnivesal with Tests

---
+++ DigitBridge.CommerceCentral.AzureStorage/AzureUtil.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.AzureStorage
+{
+    public static class AzureUtil
+    {
+        public static T QueueMessageToEntity<T>(this QueueMessage message) where T : IQueueEntity
+        {
+            var entity= JsonConvert.DeserializeObject<T>(message.MessageText);
+            entity.MessageId = message.MessageId;
+            entity.PopReceipt = message.PopReceipt;
+            return entity;
+        }
+    }
+}
---
+++ DigitBridge.CommerceCentral.AzureStorage/IQueueEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.AzureStorage
+{
+    public interface IQueueEntity
+    {
+        string MessageId { get; set; }
+        string PopReceipt { get; set; }
+    }
+}
