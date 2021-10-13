91054c8bd12b301a34dee9dcb2e69c480064ecd4
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2021-10-13 15:05:53 -0700

    Add Erp broker, erp quickbooks broker and erp event api project.

---
+++ DigitBridge.Base.Utility/Enums/ErpEventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DigitBridge.Base.Common
+{
+    public enum ErpEventType : int
+    {
+        [Description("CentralOrder To SalesOrder")]
+        CentralOrderToSalesOrder = 1,
+        [Description("Shipment To Invoice")]
+        ShipmentToInvoice  = 1,
+    }
+}
