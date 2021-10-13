5b609c1ef41bca777c3599bd4b963fdaf761bbe9
junxian <junxian.cui@hurraymart.com>
2021-10-13 12:05:55 +0800

    remove delete api from invoice remove void api from return and payment

--- DigitBridge.CommerceCentral.ERPApi/Api/Qbo/QboRefundApi.cs
+++ DigitBridge.CommerceCentral.ERPApi/Api/Qbo/QboRefundApi.cs
@@ -70,31 +70,31 @@ namespace DigitBridge.CommerceCentral.ERPApi.Api
             return new JsonNetResponse<QboRefundPayload>(payload);
         }
 
-        /// <summary>
-        /// void qbo Refund 
-        /// </summary>
-        /// <param name="req"></param>
-        /// <param name="invoiceNumber"></param>
-        /// <param name="transNum"></param>
-        /// <returns></returns>
-        [FunctionName(nameof(VoidQboRefund))]
-        [OpenApiOperation(operationId: "VoidQboRefund", tags: new[] { "Quick Books Refunds" }, Summary = "Void quick book Refund by erp invoice number and transNum.")]
-        [OpenApiParameter(name: "masterAccountNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "MasterAccountNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "profileNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "API Keys", Description = "Azure Function App key", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "invoiceNumber", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "invoiceNumber", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "transNum", In = ParameterLocation.Path, Required = true, Type = typeof(int), Summary = "transNum", Description = "Transaction Num. ", Visibility = OpenApiVisibilityType.Advanced)]
-        public static async Task<JsonNetResponse<QboRefundPayload>> VoidQboRefund(
-            [HttpTrigger(AuthorizationLevel.Function, "Patch", Route = "quickBooksRefunds/{invoiceNumber}/{transNum}")] HttpRequest req,
-            string invoiceNumber, int transNum)
-        {
-            var payload = await req.GetParameters<QboRefundPayload>();
-            var dataBaseFactory = await MyAppHelper.CreateDefaultDatabaseAsync(payload);
-            var service = new QboRefundService(payload, dataBaseFactory);
-            payload.Success = await service.VoidQboRefundAsync(invoiceNumber, transNum);
-            payload.Messages = service.Messages;
-            return new JsonNetResponse<QboRefundPayload>(payload);
-        }
+        ///// <summary>
+        ///// void qbo Refund 
+        ///// </summary>
+        ///// <param name="req"></param>
+        ///// <param name="invoiceNumber"></param>
+        ///// <param name="transNum"></param>
+        ///// <returns></returns>
+        //[FunctionName(nameof(VoidQboRefund))]
+        //[OpenApiOperation(operationId: "VoidQboRefund", tags: new[] { "Quick Books Refunds" }, Summary = "Void quick book Refund by erp invoice number and transNum.")]
+        //[OpenApiParameter(name: "masterAccountNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "MasterAccountNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
+        //[OpenApiParameter(name: "profileNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
+        //[OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "API Keys", Description = "Azure Function App key", Visibility = OpenApiVisibilityType.Advanced)]
+        //[OpenApiParameter(name: "invoiceNumber", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "invoiceNumber", Visibility = OpenApiVisibilityType.Advanced)]
+        //[OpenApiParameter(name: "transNum", In = ParameterLocation.Path, Required = true, Type = typeof(int), Summary = "transNum", Description = "Transaction Num. ", Visibility = OpenApiVisibilityType.Advanced)]
+        //public static async Task<JsonNetResponse<QboRefundPayload>> VoidQboRefund(
+        //    [HttpTrigger(AuthorizationLevel.Function, "Patch", Route = "quickBooksRefunds/{invoiceNumber}/{transNum}")] HttpRequest req,
+        //    string invoiceNumber, int transNum)
+        //{
+        //    var payload = await req.GetParameters<QboRefundPayload>();
+        //    var dataBaseFactory = await MyAppHelper.CreateDefaultDatabaseAsync(payload);
+        //    var service = new QboRefundService(payload, dataBaseFactory);
+        //    payload.Success = await service.VoidQboRefundAsync(invoiceNumber, transNum);
+        //    payload.Messages = service.Messages;
+        //    return new JsonNetResponse<QboRefundPayload>(payload);
+        //}
 
         /// <summary>
         /// get qbo Refund 
