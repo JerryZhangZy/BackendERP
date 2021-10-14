0b4111b9f3cf48f2ea4beb60eac2d10ccd484d4e
wenjian <jian.wen@hurraymart.com>
2021-10-14 16:09:26 +0800

    add erpevent api

--- DigitBridge.Base.Utility/Enums/ErpEventType.cs
+++ DigitBridge.Base.Utility/Enums/ErpEventType.cs
@@ -10,6 +10,40 @@ namespace DigitBridge.Base.Common
         [Description("CentralOrder To SalesOrder")]
         CentralOrderToSalesOrder = 1,
         [Description("Shipment To Invoice")]
-        ShipmentToInvoice  = 1,
+        ShipmentToInvoice ,
+
+        [Description("Add QuickBooks Invoice")]
+        AddQboInvoice ,
+        [Description("Update QuickBooks Invoice")]
+        UpdateQboInvoice ,
+        [Description("Void QuickBooks Invoice")]
+        VoidQboInvoice ,
+
+        [Description("Add QuickBooks InvoicePayment")]
+        AddQboInvoicePayment,
+        [Description("Delete QuickBooks InvoicePayment")]
+        DeleteQboInvoicePayment,
+
+        [Description("Add QuickBooks InvoiceReturn")]
+        AddQboInvoiceReturn,
+        [Description("Delete QuickBooks InvoiceReturn")]
+        DeleteQboInvoiceReturn,
+
+    }
+    public static class ErpEventQueueName 
+    {
+        //CentralOrderToSalesOrder = 1,
+        //ShipmentToInvoice ,
+
+        public static string AddQboInvoice = "erp-qboinvoice-add-queue";
+        public static string UpdateQboInvoice = "erp-qboinvoice-update-queue";
+        public static string VoidQboInvoice = "erp-qboinvoice-void-queue";
+
+        public static string AddQboInvoicePayment = "erp-qboinvoicepayment-add-queue";
+        public static string DeleteQboInvoicePayment = "erp-qboinvoicepayment-delete-queue";
+
+        public static string AddQboInvoiceReturn = "erp-qboinvoicereturn-add-queue";
+        public static string DeleteQboInvoiceReturn = "erp-qboinvoicereturn-delete-queue";
+
     }
 }
---
+++ DigitBridge.Base.Utility/Model/QueueMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.Base.Utility.Model
+{
+    public class QueueMessage
+    {
+        public int  DatabaseNum{get;set;}
+
+        public int MasterAccountNum { get; set; }
+
+        public int ProfileNum { get; set; }
+
+        public string EventUuid { get; set; }
+
+        public int ERPEventType { get; set; }
+
+        public string ProcessSource { get; set; }
+
+        public string ProcessUuid { get; set; }
+
+        public string ProcessData { get; set; }
+    }
+}
