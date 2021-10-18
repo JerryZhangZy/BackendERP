774cdf749a6d00bdb50529c5e217373cb2d3258f
junxian <junxian.cui@hurraymart.com>
2021-10-18 14:53:55 +0800

    Revert

---
+++ DigitBridge.CommerceCentral.ERPMdl/arInquiry/SalesOrderSummaryInquiry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.ERPMdl.arInquiry
+{
+    public class SalesOrderSummaryInquiry
+    {
+    }
+}
