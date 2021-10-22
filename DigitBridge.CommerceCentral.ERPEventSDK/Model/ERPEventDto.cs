d72cd608ea8d4407e42888c720ef6796de7dcca9
wenjian <jian.wen@hurraymart.com>
2021-10-22 15:02:05 +0800

    add Create ErpEventClientSDK project without tester;

---
+++ DigitBridge.CommerceCentral.ERPEventSDK/Model/ERPEventDto.cs
@@ -0,0 +1,35 @@
+﻿using DigitBridge.Base.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.ERPEventSDK
+{
+    public class AddErpEventDto
+    {
+        public int MasterAccountNum { get; set; }
+
+        public int ProfileNum { get; set; }
+
+        public string ProcessUuid { get; set; }
+                
+        //public string ProcessData { get; set; }
+
+        //public string ProcessSource { get; set; }
+
+    }
+
+    public class UpdateErpEventDto
+    {
+        public int MasterAccountNum { get; set; }
+
+        public int ProfileNum { get; set; }
+
+        public string EventUuid { get; set; }
+
+        public int ActionStatus { get; set; }
+
+        public string EventMessage { get; set; }
+    }
+
+}
