35763047d02aa73e50e629575592cdf48b8a331b
junxian <junxian.cui@hurraymart.com>
2021-10-24 23:11:07 +0800

    update index.

--- DigitBridge.CommerceCentral.ERPDatabase/ar/InvoiceHeaderInfo_Update.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/ar/InvoiceHeaderInfo_Update.sql
@@ -1,13 +1,45 @@
 ﻿
---TODO put frequently used filter columns in this index.
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_Complex')
-CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_Complex] ON [dbo].[InvoiceHeaderInfo]
-( 
-	[CentralFulfillmentNum] ASC,
-	[OrderShipmentNum] ASC, 
-	[CentralOrderNum]ASC,
-	[ChannelNum] ASC ,
-	[ChannelOrderID]ASC ,
-	[ShipToName]ASC 
+--This columns for filter.
+--[CentralFulfillmentNum] ASC,
+--[OrderShipmentNum] ASC, 
+--[CentralOrderNum]ASC,
+--[ChannelNum] ASC ,
+--[ChannelOrderID]ASC ,
+--[ShipToName]ASC 
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_CentralFulfillmentNum')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_CentralFulfillmentNum] ON [dbo].[InvoiceHeaderInfo]
+(
+	[CentralFulfillmentNum] ASC
 ) 
-GO
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_OrderShipmentNum')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_OrderShipmentNum] ON [dbo].[InvoiceHeaderInfo]
+(
+	[OrderShipmentNum] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_CentralOrderNum')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_CentralOrderNum] ON [dbo].[InvoiceHeaderInfo]
+(
+	[CentralOrderNum] ASC
+) 
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_ChannelNum')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_ChannelNum] ON [dbo].[InvoiceHeaderInfo]
+(
+	[ChannelNum] ASC
+)
+
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_ChannelOrderID')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_ChannelOrderID] ON [dbo].[InvoiceHeaderInfo]
+(
+	[ChannelOrderID] ASC
+)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[InvoiceHeaderInfo]') AND name = N'IX_InvoiceHeaderInfo_ShipToName')
+CREATE NONCLUSTERED INDEX [IX_InvoiceHeaderInfo_ShipToName] ON [dbo].[InvoiceHeaderInfo]
+(
+	[ShipToName] ASC
+)
