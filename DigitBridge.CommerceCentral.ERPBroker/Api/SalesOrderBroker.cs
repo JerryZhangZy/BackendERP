8d31af41470a9727bc6fd14f267db09d693c54c7
wenjian <jian.wen@hurraymart.com>
2021-10-25 08:50:54 +0800

    used EverEventSdk Instead ErpEventClientHelper

--- DigitBridge.CommerceCentral.ERPBroker/Api/SalesOrderBroker.cs
+++ DigitBridge.CommerceCentral.ERPBroker/Api/SalesOrderBroker.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.ApiCommon;
 using DigitBridge.CommerceCentral.ERPDb;
+using DigitBridge.CommerceCentral.ERPEventSDK;
+using DigitBridge.CommerceCentral.ERPEventSDK.ApiClient;
 using DigitBridge.CommerceCentral.ERPMdl;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -18,24 +20,34 @@ namespace DigitBridge.CommerceCentral.ERPBroker
         public static async Task CreateSalesOrderByCentralOrder([QueueTrigger("erp-create-salesorder-by-centralorder")] string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-            ERPQueueMessage message = null;
+            var salesOrderClient = new SalesOrderClient();
+            var eventDto = new UpdateErpEventDto();
             try
             {
-                message = JsonConvert.DeserializeObject<ERPQueueMessage>(myQueueItem);
+                ERPQueueMessage message = JsonConvert.DeserializeObject<ERPQueueMessage>(myQueueItem);
                 var payload = new SalesOrderPayload()
                 {
                     MasterAccountNum = message.MasterAccountNum,
                     ProfileNum = message.ProfileNum
                 };
+                eventDto.EventUuid = message.EventUuid;
+                eventDto.MasterAccountNum = message.MasterAccountNum;
+                eventDto.ProfileNum = message.ProfileNum;
                 var dbFactory = await MyAppHelper.CreateDefaultDatabaseAsync(payload);
                 var svc = new SalesOrderManager(dbFactory);
                 (bool ret, List<string> salesOrderUuids) = await svc.CreateSalesOrderByChannelOrderIdAsync(message.ProcessUuid);
 
-                ErpEventClientHelper.UpdateEventERPAsync(ret, message, svc.Messages.ObjectToString());
+                eventDto.ActionStatus = ret ? 0 : 1;
+                eventDto.EventMessage = svc.Messages.ObjectToString();
             }
             catch (Exception e)
             {
-                ErpEventClientHelper.UpdateEventERPAsync(false, message, e.ObjectToString());
+                eventDto.ActionStatus = 1;
+                eventDto.EventMessage = e.ObjectToString();
+            }
+            finally
+            {
+                await salesOrderClient.SendActionResultAsync(eventDto);
             }
         }
     }
