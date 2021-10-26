ab1221b0df4c97e1c92bdcc4fa8d153dd7cad891
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2021-10-25 17:07:16 -0700

    Compile

--- DigitBridge.CommerceCentral.ERPMdl/selectList/so/so_customerCode.cs
+++ DigitBridge.CommerceCentral.ERPMdl/selectList/so/so_customerCode.cs
@@ -1,6 +1,18 @@
-﻿using Mastersystem.Emaster.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
-namespace Mastersystem.Emaster.Dao.Ado
+using DigitBridge.Base.Utility;
+using DigitBridge.CommerceCentral.YoPoco;
+using DigitBridge.CommerceCentral.ERPDb;
+using Microsoft.AspNetCore.Http;
+
+namespace DigitBridge.CommerceCentral.ERPMdl
 {
     public partial class so_customerCode : DemListAdoBase
     {
