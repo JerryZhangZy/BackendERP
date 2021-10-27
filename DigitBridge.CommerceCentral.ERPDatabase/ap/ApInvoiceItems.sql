218a92c261d37895948e7dca3c1725e87db13827
正家 王 <zhengjia.wang@hurraymart.com>
2021-10-27 19:22:55 +0800

    1.modify ApInvoiceHeader.sql ,ApInvoiceHeaderAttributes.sql,ApInvoiceHeaderInfo.sql,ApInvoiceItems.sql,ApInvoiceTransaction.sql 2.run t4 3.add ApInvoiceApi.cs

--- DigitBridge.CommerceCentral.ERPDatabase/ap/ApInvoiceItems.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/ap/ApInvoiceItems.sql
@@ -14,24 +14,24 @@
 	[Description] NVarchar(200) NOT NULL,--ApInvoice item description 
 	[Notes] NVarchar(500) NOT NULL,--ApInvoice item notes 
 
-	[Currency] VARCHAR(10) NULL,
+	[Currency] VARCHAR(10) NULL,--(Ignore)  
 	[Amount] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Item total amount. 
 	[IsAp] TINYINT NOT NULL DEFAULT 1,--ApInvoice item will add to invoice total amount
 
 	[CreditAccount] BIGINT NULL DEFAULT 0, --G/L Credit account
 	[DebitAccount] BIGINT NULL DEFAULT 0, --G/L Debit account , A/P invoice distribution should specify G/L Debit account
 
-    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()),
-    [UpdateDateUtc] DATETIME NULL,
-    [EnterBy] Varchar(100) NOT NULL,
-    [UpdateBy] Varchar(100) NOT NULL,
-    [DigitBridgeGuid] uniqueidentifier NOT NULL DEFAULT (newid()),
+    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()),--(Ignore) 
+    [UpdateDateUtc] DATETIME NULL,--(Ignore) 
+    [EnterBy] Varchar(100) NOT NULL,--(Ignore) 
+    [UpdateBy] Varchar(100) NOT NULL,--(Ignore) 
+    [DigitBridgeGuid] uniqueidentifier NOT NULL DEFAULT (newid()),--(Ignore) 
     CONSTRAINT [PK_ApInvoiceItems] PRIMARY KEY ([RowNum]), 
 ) ON [PRIMARY]
 GO
 
 --IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ApInvoiceItems]') AND name = N'UI_ApInvoiceItems_ApInvoiceItemsId')
-CREATE UNIQUE NONCLUSTERED INDEX [UI_ApInvoiceItems_ApInvoiceItemsUuid] ON [dbo].[ApInvoiceItems]
+CREATE UNIQUE NONCLUSTERED INDEX [UK_ApInvoiceItems_ApInvoiceItemsUuid] ON [dbo].[ApInvoiceItems]
 (
 	[ApInvoiceItemsUuid] ASC
 ) ON [PRIMARY]
@@ -48,7 +48,7 @@ GO
 --IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ApInvoiceItems]') AND name = N'IX_ApInvoiceItems_ApInvoiceId')
 CREATE NONCLUSTERED INDEX [IX_ApInvoiceItems_ApInvoiceId] ON [dbo].[ApInvoiceItems]
 (
-	[ApInvoiceId] ASC
+	[ApInvoiceUuid] ASC
 ) ON [PRIMARY]
 GO
 
--- DigitBridge.CommerceCentral.ERPDatabase/ap/ApInvoiceTransaction.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/ap/ApInvoiceTransaction.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ApInvoiceTransaction]
 (
-	[RowNum] BIGINT IDENTITY(1,1) NOT NULL,
+	[RowNum] BIGINT IDENTITY(1,1) NOT NULL, --(Readonly) Record Number. Required, <br> Display: false, Editable: false.
     [TransUuid] VARCHAR(50) NOT NULL DEFAULT (CAST(newid() AS NVARCHAR(50))), --Global Unique Guid for ApInvoice Transaction
     [TransNum] INT NOT NULL DEFAULT 1, --Transaction number
 
@@ -17,24 +17,24 @@
 	[CheckNum] VARCHAR(100) NULL, --Check number
 	[AuthCode] VARCHAR(100) NULL, --Auth code from merchant bank
 
-	[Currency] VARCHAR(10) NULL,
+	[Currency] VARCHAR(10) NULL,--(Ignore) 
 	[ExchangeRate] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Realtime Exchange Rate when process transaction
 	[Amount] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Total order amount. Include every charge. Related to VAT. For US orders, tax should not be included. Refer to tax info to find more detail. Reference calculation 
 
 	[CreditAccount] BIGINT NULL DEFAULT 0, --G/L Credit account
 	[DebitAccount] BIGINT NULL DEFAULT 0, --G/L Debit account
 
-    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()),
-    [UpdateDateUtc] DATETIME NULL,
-    [EnterBy] Varchar(100) NOT NULL,
-    [UpdateBy] Varchar(100) NOT NULL,
-    [DigitBridgeGuid] uniqueidentifier NOT NULL DEFAULT (newid()),
+    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()),--(Ignore) 
+    [UpdateDateUtc] DATETIME NULL,--(Ignore) 
+    [EnterBy] Varchar(100) NOT NULL,--(Ignore) 
+    [UpdateBy] Varchar(100) NOT NULL,--(Ignore) 
+    [DigitBridgeGuid] uniqueidentifier NOT NULL DEFAULT (newid()),--(Ignore) 
     CONSTRAINT [PK_ApInvoiceTransaction] PRIMARY KEY ([RowNum]), 
 ) ON [PRIMARY]
 GO
 
 --IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ApInvoiceTransaction]') AND name = N'UI_ApInvoiceTransaction_ApInvoiceId')
-CREATE UNIQUE NONCLUSTERED INDEX [UI_ApInvoiceTransaction_TransUuid] ON [dbo].[ApInvoiceTransaction]
+CREATE UNIQUE NONCLUSTERED INDEX [UK_ApInvoiceTransaction_TransUuid] ON [dbo].[ApInvoiceTransaction]
 (
 	[TransUuid] ASC
 ) ON [PRIMARY]
@@ -50,7 +50,7 @@ GO
 --IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ApInvoiceTransaction]') AND name = N'UI_ApInvoiceTransaction_TransNum')
 CREATE NONCLUSTERED INDEX [IX_ApInvoiceTransaction_TransNum] ON [dbo].[ApInvoiceTransaction]
 (
-	[ApInvoiceId] ASC,
+	[ApInvoiceUuid] ASC,
 	[TransNum] ASC
 ) ON [PRIMARY]
 GO
