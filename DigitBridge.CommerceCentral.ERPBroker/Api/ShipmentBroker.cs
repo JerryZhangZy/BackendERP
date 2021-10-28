94db30765a2f82d094fbed291e9cfdfc20bc243a
junxian <junxian.cui@hurraymart.com>
2021-10-28 12:35:36 +0800

    Remove useless code

--- DigitBridge.CommerceCentral.ERPBroker/Api/ShipmentBroker.cs
+++ DigitBridge.CommerceCentral.ERPBroker/Api/ShipmentBroker.cs
@@ -18,32 +18,6 @@ namespace DigitBridge.CommerceCentral.ERPBroker
     [ApiFilter(typeof(ShipmentBroker))]
     public static class ShipmentBroker
     {
-        //[FunctionName("CreateShipment")]
-        //public static async Task CreateShipment([QueueTrigger(QueueName.Erp_Shipment_Queue)] string myQueueItem, ILogger log)
-        //{
-        //    var client = new ShipmentClient();
-        //    var message = JsonConvert.DeserializeObject<ERPQueueMessage>(myQueueItem);
-        //    try
-        //    {
-        //        var payload = new OrderShipmentPayload()
-        //        {
-        //            MasterAccountNum = message.MasterAccountNum,
-        //            ProfileNum = message.ProfileNum,
-        //            OrderShipmentUuids = new List<string>() { message.ProcessUuid }
-        //        };
-
-        //        var dbFactory = await MyAppHelper.CreateDefaultDatabaseAsync(payload);
-        //        var service = new OrderShipmentManager(dbFactory);
-        //        (bool success, List<string> salesOrderUuids) = await service.CreateShipmentFromPayloadAsync(payload);
-
-        //        client.SendActionResultAsync(message, service.Messages.ObjectToString(), success);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        client.SendActionResultAsync(message, e.ObjectToString());
-        //    }
-        //}
-
         /// <summary>
         /// CreateShipment
         /// </summary>
