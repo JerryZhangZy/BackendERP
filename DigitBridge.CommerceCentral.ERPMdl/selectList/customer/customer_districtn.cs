61d2a5e3222e013a6ad1da5ec3e4942ca52db38c
正家 王 <zhengjia.wang@hurraymart.com>
2021-10-31 15:52:11 +0800

    add selectList.customer

---
+++ DigitBridge.CommerceCentral.ERPMdl/selectList/customer/customer_districtn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+using DigitBridge.Base.Utility;
+using DigitBridge.CommerceCentral.YoPoco;
+using DigitBridge.CommerceCentral.ERPDb;
+using Microsoft.AspNetCore.Http;
+namespace DigitBridge.CommerceCentral.ERPMdl.selectList.customer
+{
+ 
+    public partial class customer_districtn : SelectListBase
+    {
+        public override string Name => "customer_districtn";
+
+        public customer_districtn(IDataBaseFactory dbFactory) : base(dbFactory) { }
+
+        protected override void SetFilterSqlString()
+        {
+            this.QueryObject.LoadAll = false;
+            if (string.IsNullOrEmpty(this.QueryObject.Term.FilterValue))
+                this.QueryObject.SetTermSqlString(
+                    $"COALESCE(tbl.Districtn, '') LIKE '{this.QueryObject.Term.FilterValue.ToSqlSafeString()}%' "
+                );
+            else
+                this.QueryObject.SetTermSqlString(null);
+        }
+
+        protected override string GetSQL_select()
+        {
+            this.SetFilterSqlString();
+            this.SQL_Select = $@"
+ 
+    SELECT 
+      tbl.Districtn AS [value],
+      '' AS[text],
+        COUNT(1) AS [count] FROM
+      [dbo].[Customer] tbl
+   
+    WHERE COALESCE(tbl.Districtn,'') != '' 
+        AND {this.QueryObject.GetSQL()}
+    GROUP BY tbl.Districtn
+";
+            return this.SQL_Select;
+        }
+    }
+}
---
+++ DigitBridge.CommerceCentral.ERPMdl/selectList/customer/customer_divisionCode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+using DigitBridge.Base.Utility;
+using DigitBridge.CommerceCentral.YoPoco;
+using DigitBridge.CommerceCentral.ERPDb;
+using Microsoft.AspNetCore.Http;
+namespace DigitBridge.CommerceCentral.ERPMdl.selectList.customer
+{
+ 
+    public partial class customer_divisionCode : SelectListBase
+    {
+        public override string Name => "customer_divisionCode";
+
+        public customer_divisionCode(IDataBaseFactory dbFactory) : base(dbFactory) { }
+
+        protected override void SetFilterSqlString()
+        {
+            this.QueryObject.LoadAll = false;
+            if (string.IsNullOrEmpty(this.QueryObject.Term.FilterValue))
+                this.QueryObject.SetTermSqlString(
+                    $"COALESCE(tbl.DivisionCode, '') LIKE '{this.QueryObject.Term.FilterValue.ToSqlSafeString()}%' "
+                );
+            else
+                this.QueryObject.SetTermSqlString(null);
+        }
+
+        protected override string GetSQL_select()
+        {
+            this.SetFilterSqlString();
+            this.SQL_Select = $@"
+ 
+    SELECT 
+      tbl.DivisionCode AS [value],
+      '' AS[text],
+        COUNT(1) AS [count] FROM
+      [dbo].[Customer] tbl
+   
+    WHERE COALESCE(tbl.DivisionCode,'') != '' 
+        AND {this.QueryObject.GetSQL()}
+    GROUP BY tbl.DivisionCode
+";
+            return this.SQL_Select;
+        }
+    }
+}
