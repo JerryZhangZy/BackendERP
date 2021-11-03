26dde0fddd5f96241fc1d4396f41ffad5aa4760b
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2021-11-03 11:24:40 -0700

    Fix bugs for Json.Net reference, Add swagger to DataEntryCodeApi

--- DigitBridge.CommerceCentral.ERPDb/Model/ItemCostClass.cs
+++ DigitBridge.CommerceCentral.ERPDb/Model/ItemCostClass.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using System.Xml.Serialization;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.YoPoco;
 
