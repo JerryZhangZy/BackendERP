65ff6c9ae017868f6a61fdeaf38ab521686695e8
junxian <junxian.cui@hurraymart.com>
2021-11-03 17:35:46 +0800

    fix sample data bug.

--- DigitBridge.CommerceCentral.ERPBroker/OpenApiModel/SalesOrderOpenListPayloadOpenApi.cs
+++ DigitBridge.CommerceCentral.ERPBroker/OpenApiModel/SalesOrderOpenListPayloadOpenApi.cs
@@ -74,7 +74,7 @@ namespace DigitBridge.CommerceCentral.ERPBroker
 
         //public SalesOrderStatus OrderStatus { get; set; }
 
-        public SalesOrderType OrderType { get; set; }
+        public int OrderType { get; set; }
 
         public string CustomerCode { get; set; }
         public string CustomerName { get; set; }
@@ -92,7 +92,7 @@ namespace DigitBridge.CommerceCentral.ERPBroker
                 .RuleFor(u => u.OrderDateTo, f => f.Date.Past(0).Date)
                 .RuleFor(u => u.UpdateDateUtcFrom, f => f.Date.Past(0).Date.AddDays(-30))
                 .RuleFor(u => u.UpdateDateUtcTo, f => f.Date.Past(0).Date)
-                .RuleFor(u => u.OrderType, f => SalesOrderType.ChannelOrder)
+                .RuleFor(u => u.OrderType, f => (int)SalesOrderType.ChannelOrder)
                 .RuleFor(u => u.CustomerCode, f => string.Empty)
                 .RuleFor(u => u.CustomerName, f => string.Empty)
                 //.RuleFor(u => u.OrderStatus, f => (int)SalesOrderStatus.Open) 
