cdf9837a5edd7a3053a119ffdd8d000053416aff
junxian <junxian.cui@hurraymart.com>
2021-11-04 15:17:03 +0800

    Mapper erp S/O to wms S/O

--- DigitBridge.CommerceCentral.ERPMdl/so/SalesOrderOpenList.cs
+++ DigitBridge.CommerceCentral.ERPMdl/so/SalesOrderOpenList.cs
@@ -28,52 +28,94 @@ namespace DigitBridge.CommerceCentral.ERPMdl
         {
         }
 
-        #region select TODO pick necessary table and columns.
+        #region get select columns
         protected string GetHeader_Columns()
         {
+            var header = "WMSOrderHeader";
             var columns = $@"
-{Helper.SelectAll(Helper.TableAllies, Helper.TableName)},
-COALESCE(ordtp.text, '') as '{Helper.TableName}.OrderTypeText',  
-COALESCE(ordst.text, '') as '{Helper.TableName}.OrderStatusText'
+
+{InfoHelper.TableAllies}.WarehouseCode as '{header}.WarehouseCode',
+{Helper.TableAllies}.DatabaseNum as '{header}.CentralDatabaseNum',
+{InfoHelper.TableAllies}.CentralOrderNum as '{header}.CentralOrderNum',
+{InfoHelper.TableAllies}.ChannelNum as '{header}.ChannelNum',
+{InfoHelper.TableAllies}.ChannelAccountNum as '{header}.ChannelAccountNum',
+channelAccount.ChannelAccountName as '{header}.ChannelAccountName',
+{InfoHelper.TableAllies}.ChannelOrderId as '{header}.ChannelOrderId',
+{InfoHelper.TableAllies}.SecondaryChannelOrderId as '{header}.SecondaryChannelOrderId',
+{Helper.TableAllies}.OrderNumber as '{header}.SellerOrderId',
+{Helper.TableAllies}.Currency as '{header}.Currency',
+{Helper.TableAllies}.OrderDate as '{header}.OriginalOrderDate',
+{InfoHelper.TableAllies}.Notes as '{header}.SellerPrivateNotes',
+{Helper.TableAllies}.TotalAmount as '{header}.TotalOrderAmount',
+{Helper.TableAllies}.TaxAmount as '{header}.TotalTaxAmount',
+{Helper.TableAllies}.ShippingAmount as '{header}.TotalShippingAmount',
+{Helper.TableAllies}.ShippingTaxAmount as '{header}.TotalShippingTaxAmount',
+{InfoHelper.TableAllies}.ShipToName as '{header}.ShipToName',
+{InfoHelper.TableAllies}.ShipToFirstName as '{header}.ShipToFirstName',
+{InfoHelper.TableAllies}.ShipToLastName as '{header}.ShipToLastName',
+{InfoHelper.TableAllies}.ShipToSuffix as '{header}.ShipToSuffix',
+{InfoHelper.TableAllies}.ShipToCompany as '{header}.ShipToCompany',
+{InfoHelper.TableAllies}.ShipToCompanyJobTitle as '{header}.ShipToCompanyJobTitle',
+{InfoHelper.TableAllies}.ShipToAttention as '{header}.ShipToAttention',
+{InfoHelper.TableAllies}.ShipToDaytimePhone as '{header}.ShipToDaytimePhone',
+{InfoHelper.TableAllies}.ShipToNightPhone as '{header}.ShipToNightPhone',
+{InfoHelper.TableAllies}.ShipToAddressLine1 as '{header}.ShipToAddressLine1',
+{InfoHelper.TableAllies}.ShipToAddressLine2 as '{header}.ShipToAddressLine2',
+{InfoHelper.TableAllies}.ShipToAddressLine3 as '{header}.ShipToAddressLine3',
+{InfoHelper.TableAllies}.ShipToCity as '{header}.ShipToCity',
+{InfoHelper.TableAllies}.ShipToState as '{header}.ShipToState',
+{InfoHelper.TableAllies}.ShipToStateFullName as '{header}.ShipToStateFullName',
+{InfoHelper.TableAllies}.ShipToPostalCode as '{header}.ShipToPostalCode',
+{InfoHelper.TableAllies}.ShipToPostalCodeExt as '{header}.ShipToPostalCodeExt',
+{InfoHelper.TableAllies}.ShipToCounty as '{header}.ShipToCounty',
+{InfoHelper.TableAllies}.ShipToCountry as '{header}.ShipToCountry',
+{InfoHelper.TableAllies}.ShipToEmail as '{header}.ShipToEmail',
+{InfoHelper.TableAllies}.BillToName as '{header}.BillToName',
+{InfoHelper.TableAllies}.BillToFirstName as '{header}.BillToFirstName',
+{InfoHelper.TableAllies}.BillToLastName as '{header}.BillToLastName',
+{InfoHelper.TableAllies}.BillToSuffix as '{header}.BillToSuffix',
+{InfoHelper.TableAllies}.BillToCompany as '{header}.BillToCompany',
+{InfoHelper.TableAllies}.BillToCompanyJobTitle as '{header}.BillToCompanyJobTitle',
+{InfoHelper.TableAllies}.BillToAttention as '{header}.BillToAttention',
+{InfoHelper.TableAllies}.BillToAddressLine1 as '{header}.BillToAddressLine1',
+{InfoHelper.TableAllies}.BillToAddressLine2 as '{header}.BillToAddressLine2',
+{InfoHelper.TableAllies}.BillToAddressLine3 as '{header}.BillToAddressLine3',
+{InfoHelper.TableAllies}.BillToCity as '{header}.BillToCity',
+{InfoHelper.TableAllies}.BillToState as '{header}.BillToState',
+{InfoHelper.TableAllies}.BillToStateFullName as '{header}.BillToStateFullName',
+{InfoHelper.TableAllies}.BillToPostalCode as '{header}.BillToPostalCode',
+{InfoHelper.TableAllies}.BillToPostalCodeExt as '{header}.BillToPostalCodeExt',
+{InfoHelper.TableAllies}.BillToCounty as '{header}.BillToCounty',
+{InfoHelper.TableAllies}.BillToCountry as '{header}.BillToCountry',
+{InfoHelper.TableAllies}.BillToEmail as '{header}.BillToEmail',
+{InfoHelper.TableAllies}.BillToDaytimePhone as '{header}.BillToDaytimePhone',
+{InfoHelper.TableAllies}.BillToNightPhone as '{header}.BillToNightPhone'
 ";
-            return columns;
-        }
-        protected string GetHeaderInfo_Columns()
-        {
-            var columns = $@"
-{InfoHelper.SelectAll(InfoHelper.TableAllies, InfoHelper.TableName)},
-chanel.ChannelName as '{InfoHelper.TableName}.ChannelName',
-channelAccount.ChannelAccountName as '{InfoHelper.TableName}.ChannelAccountName' 
-";
-            return columns;
-        }
-        protected string GetHeaderAttr_Columns()
-        {
-            var columns = $@"
-{InfoAttrHelper.SelectAll(InfoAttrHelper.TableAllies, InfoAttrHelper.TableName)} 
-";
-            return columns;
-        }
-        protected string GetItemWithItemAttr_Columns()
-        {
-            var columns = $@"
-( 
-SELECT 
- {ItemHelper.SelectAll(ItemHelper.TableAllies)}
-,{ItemAttrHelper.SelectAll(ItemAttrHelper.TableAllies, ItemAttrHelper.TableName)} 
-FROM { ItemHelper.TableName} { ItemHelper.TableAllies}
-LEFT JOIN { ItemAttrHelper.TableName} { ItemAttrHelper.TableAllies} ON({ ItemAttrHelper.TableAllies}.SalesOrderItemsUuid = { ItemHelper.TableAllies}.SalesOrderItemsUuid)
-WHERE { ItemHelper.TableAllies}.SalesOrderUuid = { Helper.TableAllies}.SalesOrderUuid 
-FOR JSON PATH
-) AS SalesOrderItems";
             return columns;
         }
         protected string GetItem_Columns()
         {
+            var item = "WMSOrderLine";
+
+            var columns = $@"
+{ItemHelper.TableAllies}.SKU as '{item}.SKU',
+{ItemHelper.TableAllies}.OrderQty as '{item}.OrderQty',
+{ItemHelper.TableAllies}.ShipQty as '{item}.ShipQty',
+{ItemHelper.TableAllies}.CancelledQty as '{item}.CancelQty',
+{ItemHelper.TableAllies}.Price as '{item}.UnitPrice',
+{ItemHelper.TableAllies}.TaxAmount as '{item}.LineItemTaxAmount',
+{ItemHelper.TableAllies}.ShippingAmount as '{item}.LineShippingAmount',
+{ItemHelper.TableAllies}.ShippingTaxAmount as '{item}.LineShippingTaxAmount',
+{ItemHelper.TableAllies}.ItemDate as '{item}.EnterDate'
+";
+            return columns;
+        }
+        protected string GetItem_Script()
+        {
             var columns = $@"
 ( 
 SELECT 
- {ItemHelper.SelectAll(ItemHelper.TableAllies)} 
+{GetItem_Columns()}
 FROM { ItemHelper.TableName} { ItemHelper.TableAllies}
 WHERE { ItemHelper.TableAllies}.SalesOrderUuid = { Helper.TableAllies}.SalesOrderUuid 
 FOR JSON PATH
@@ -86,23 +128,11 @@ FOR JSON PATH
 
         protected override string GetSQL_select()
         {
-            //            // No Header attributes & item attributes.
-            //            this.SQL_Select = $@"
-            //SELECT 
-            // {GetHeader_Columns()}
-            //,{GetHeaderInfo_Columns()}
-            //,{GetItem_Columns()} 
-            //";
-
-            //with Header attributes & item attributes.
             this.SQL_Select = $@"
             SELECT 
              {GetHeader_Columns()}
-            ,{GetHeaderAttr_Columns()}
-            ,{GetHeaderInfo_Columns()}
-            ,{GetItemWithItemAttr_Columns()} 
-            ,{GetItemWithItemAttr_Columns()}
-            ";
+            ,{GetItem_Script()} 
+            "; 
 
             return this.SQL_Select;
         }
