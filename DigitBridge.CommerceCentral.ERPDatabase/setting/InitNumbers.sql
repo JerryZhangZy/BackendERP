c29dc68561600ff9fbac6da2fa15966bd95fff19
zhengjia.wang@hurraymart.com <zhengjia.wang@hurraymart.com>
2021-11-05 10:52:28 +0800

    add initnumber

--- DigitBridge.CommerceCentral.ERPDatabase/setting/InitNumbers.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/setting/InitNumbers.sql
@@ -4,7 +4,6 @@
     [DatabaseNum] INT NOT NULL, --(Readonly) Database Number. <br> Display: false, Editable: false.
 	[MasterAccountNum] INT NOT NULL, --(Readonly) Login user account. <br> Display: false, Editable: false.
 	[ProfileNum] INT NOT NULL, --(Readonly) Login user profile. <br> Display: false, Editable: false.
-
     [InitNumbersUuid] VARCHAR(50) NOT NULL DEFAULT (CAST(newid() AS NVARCHAR(50))), --Global Unique Guid
     [CustomerUuid] VARCHAR(50) NOT NULL DEFAULT '', --Customer uuid. CustomerUuid = DEFAULT for system init number <br> Display: false, Editable: false.
     [InActive] TINYINT NOT NULL DEFAULT 0, --Disable this record
@@ -12,11 +11,10 @@
 	[Number] INT NOT NULL DEFAULT 0, --Init number, real number will be more than init number and not exist number  
 	[Prefix] VARCHAR(20) NOT NULL DEFAULT '', --Prefix append to Init number 
 	[Suffix] VARCHAR(20) NOT NULL DEFAULT '', --Suffix follow by Init number
-
-    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()),
-    [UpdateDateUtc] DATETIME NULL,
-    [EnterBy] Varchar(100) NOT NULL,
-    [UpdateBy] Varchar(100) NOT NULL,
+    [EnterDateUtc] DATETIME NOT NULL DEFAULT (getutcdate()), --(Radonly) Created Date time. <br> Title: Created At, Display: true, Editable: false
+    [UpdateDateUtc] DATETIME NULL,--(Readonly) Last update date time. <br> Title: Update At, Display: true, Editable: false
+    [EnterBy] Varchar(100) NOT NULL,--(Readonly) User who created this order. <br> Title: Created By, Display: true, Editable: false
+    [UpdateBy] Varchar(100) NOT NULL,--(Readonly) Last updated user. <br> Title: Update By, Display: true, Editable: false
     [DigitBridgeGuid] uniqueidentifier NOT NULL DEFAULT (newid()),
     CONSTRAINT [PK_InitNumbers] PRIMARY KEY ([RowNum]), 
 ) ON [PRIMARY]
