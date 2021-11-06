b6fa9a85333880fc7fa9cb24eef8910b6875e119
junxian <junxian.cui@hurraymart.com>
2021-11-06 11:20:12 +0800

    Fix  online bug: Add shipment from wms shipment.

--- DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration/WMSShipmentClientTests.cs
+++ DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration/WMSShipmentClientTests.cs
@@ -15,8 +15,9 @@ namespace DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration
         protected TestFixture<StartupTest> Fixture { get; }
         public IConfiguration Configuration { get; }
 
-        private string _baseUrl = "http://localhost:7074/api/";
-        private string _code = "put online app key";
+        //private string _baseUrl = "http://localhost:7074/api/"; 
+        private string _baseUrl = "https://digitbridge-erp-integration-api-dev.azurewebsites.net/api/";
+        private string _code = "aa4QcFoSH4ADcXEROimDtbPa4h0mY/dsNFuK1GfHPAhqx5xMJRAaHw==";
         protected const int MasterAccountNum = 10001;
         protected const int ProfileNum = 10001;
 
@@ -50,6 +51,7 @@ namespace DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration
         {
             var client = new WMSShipmentClient(_baseUrl, _code);
             var shipment = GetWmsShipment();
+            var jsonData = JsonConvert.SerializeObject(shipment);
             var success = await client.AddSingleShipmentAsync(MasterAccountNum, ProfileNum, shipment);
             Assert.True(client.ResopneData != null, "Invoke sdk method (AddSingleShipmentAsync) failed.");
             Assert.True(success, $"SDK invoice succeed. You can try CreateShipmentAsync_Test to test logic,Logic error is{client.Messages.ObjectToString()}");
