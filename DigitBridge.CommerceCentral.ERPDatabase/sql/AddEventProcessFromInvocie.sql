9f6fcda0b20ef571428461ce132a3d516894f87f
junxian <junxian.cui@hurraymart.com>
2021-11-07 15:51:15 +0800

    Add init event process from invoice script

---
+++ DigitBridge.CommerceCentral.ERPDatabase/sql/AddEventProcessFromInvocie.sql
@@ -0,0 +1,15 @@
+﻿insert into EventProcessERP (DatabaseNum,MasterAccountNum,ProfileNum,ChannelNum,ChannelAccountNum,ERPEventProcessType,processuuid,actionstatus)
+select 
+top 10
+header.DatabaseNum,
+header.MasterAccountNum,header.ProfileNum,
+info.ChannelNum,info.ChannelAccountNum,
+1 as ERPEventProcessType,
+header.InvoiceUuid as processuuid,
+0 as actionstatus
+
+from InvoiceHeader header
+left join InvoiceHeaderInfo info on header.InvoiceUuid=info.InvoiceUuid
+order by header.RowNum desc 
+ 
+select * from EventProcessERP
