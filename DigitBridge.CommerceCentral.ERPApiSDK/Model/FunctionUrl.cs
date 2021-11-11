cd6e9d5aadc309ae64127fb67ab186fbca429ac7
junxian <junxian.cui@hurraymart.com>
2021-11-11 10:52:19 +0800

    Add Ack salesorder to sdk

--- DigitBridge.CommerceCentral.ERPApiSDK/Model/FunctionUrl.cs
+++ DigitBridge.CommerceCentral.ERPApiSDK/Model/FunctionUrl.cs
@@ -19,6 +19,8 @@ namespace DigitBridge.CommerceCentral.ERPApiSDK
         internal static string CreateInvoiceByOrderShipment = "/addCreateInvoiceByOrderShipment";
 
         internal const string GetSalesOrderOpenList = "wms/salesOrders/find";
+        internal const string AckReceiveSalesOrders = "wms/salesOrders/AckReceive";
+        internal const string AckProcessSalesOrders = "wms/salesOrders/AckProcess";
 
         internal const string GetPurchaseOrderList = "purchaseOrder/find";
         internal const string CreatePoReceive = "poReceives";
@@ -28,6 +30,6 @@ namespace DigitBridge.CommerceCentral.ERPApiSDK
 
         internal const string AddShipments = "wms/shipments";
 
-        internal const string UnprocessList= "commercecentral/invoices/list/unprocess"; 
+        internal const string UnprocessList = "commercecentral/invoices/list/unprocess";
     }
 }
