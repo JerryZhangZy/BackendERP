7bebd5157ef5d86b11b11b5f36f4d5e27ad710bd
junxian <junxian.cui@hurraymart.com>
2021-11-17 18:48:05 +0800

    Regenrate code to set utc now to UpdateDateUtc

--- DigitBridge.CommerceCentral.ERPDb/warehouseTransfer/WarehouseTransferHeader.Designer.cs
+++ DigitBridge.CommerceCentral.ERPDb/warehouseTransfer/WarehouseTransferHeader.Designer.cs
@@ -475,7 +475,7 @@ namespace DigitBridge.CommerceCentral.ERPDb
             {
 				if (value != null || AllowNull) 
 				{
-					_updateDateUtc = (value is null) ? (DateTime?) null : value?.Date.ToSqlSafeValue(); 
+					_updateDateUtc = (value is null) ? (DateTime?) null : value.ToSqlSafeValue(); 
 					OnPropertyChanged("UpdateDateUtc", value);
 				}
             }
@@ -593,6 +593,17 @@ namespace DigitBridge.CommerceCentral.ERPDb
         }
 
 
+		public override WarehouseTransferHeader ConvertDbFieldsToData()
+		{
+			base.ConvertDbFieldsToData();
+			return this;
+		}
+		public override WarehouseTransferHeader ConvertDataFieldsToDb()
+		{
+			base.ConvertDataFieldsToDb();
+			UpdateDateUtc =DateTime.UtcNow;
+			return this;
+		}
 
         #endregion Methods - Generated 
     }
