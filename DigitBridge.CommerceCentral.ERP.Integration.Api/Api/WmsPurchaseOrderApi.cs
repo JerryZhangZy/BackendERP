80f1c332290e4affb4a552398528bb393ef2e78a
junxian <junxian.cui@hurraymart.com>
2021-11-18 19:21:20 +0800

    Add Init data script for po test.

--- DigitBridge.CommerceCentral.ERP.Integration.Api/Api/WmsPurchaseOrderApi.cs
+++ DigitBridge.CommerceCentral.ERP.Integration.Api/Api/WmsPurchaseOrderApi.cs
@@ -20,14 +20,14 @@ namespace DigitBridge.CommerceCentral.ERP.Integration.Api.Api
         /// Get purchase order list by criteria.
         /// </summary>
         [FunctionName(nameof(PurchaseOrderList))]
-        [OpenApiOperation(operationId: "PurchaseOrderList", tags: new[] { "PurchaseOrders" }, Summary = "Get purchase order list by criteria.")]
+        [OpenApiOperation(operationId: "PurchaseOrderList", tags: new[] { "WMSPurchaseOrders" }, Summary = "Get purchase order list by criteria.")]
         [OpenApiParameter(name: "masterAccountNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "MasterAccountNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiParameter(name: "profileNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "API Keys", Description = "Azure Function App key", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(PurchaseOrderPayloadFind), Description = "Request Body in json format")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(PurchaseOrderPayloadFind))]
         public static async Task<JsonNetResponse<PurchaseOrderPayload>> PurchaseOrderList(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "purchaseOrders/find")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "wms/purchaseOrders/find")] HttpRequest req)
         {
             var payload = await req.GetParameters<PurchaseOrderPayload>(true);
             var dataBaseFactory = await MyAppHelper.CreateDefaultDatabaseAsync(payload);
