c34aa27dce106a4e0a57218107434495907cf4d7
junxian <junxian.cui@hurraymart.com>
2021-11-19 15:22:16 +0800

    Add columns to P/O

---
+++ DigitBridge.CommerceCentral.ERPDatabase/po/PoHeaderInfo_Update.sql
@@ -0,0 +1,6 @@
+﻿ 
+ -- 11/19/2021 added by junxian
+IF COL_LENGTH('PoHeaderInfo', 'WarehouseCode') IS NULL					
+BEGIN					
+    ALTER TABLE PoItems ADD [WarehouseCode] VARCHAR(50) NOT NULL DEFAULT ''
+END					
