a1f3256c84071c17f68ca28672f85fcfab2bd2e1
junxian <junxian.cui@hurraymart.com>
2021-11-22 09:51:21 +0800

    change get unprocess invoice sql.

--- DigitBridge.CommerceCentral.ERPMdl/ar/InvoiceUnprocessList .cs
+++ DigitBridge.CommerceCentral.ERPMdl/ar/InvoiceUnprocessList .cs
@@ -8,11 +8,7 @@ using DigitBridge.Base.Common;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.ERPDb;
 using DigitBridge.CommerceCentral.YoPoco;
-using Microsoft.Data.SqlClient;
-using Helper = DigitBridge.CommerceCentral.ERPDb.InvoiceHeaderHelper;
-using InfoHelper = DigitBridge.CommerceCentral.ERPDb.InvoiceHeaderInfoHelper;
-using ItemHelper = DigitBridge.CommerceCentral.ERPDb.InvoiceItemsHelper;
-using EventHelper = DigitBridge.CommerceCentral.ERPDb.EventProcessERPHelper;
+
 namespace DigitBridge.CommerceCentral.ERPMdl
 {
     public class InvoiceUnprocessList : SqlQueryBuilder<InvoiceUnprocessQuery>
@@ -23,97 +19,71 @@ namespace DigitBridge.CommerceCentral.ERPMdl
         public InvoiceUnprocessList(IDataBaseFactory dbFactory, InvoiceUnprocessQuery queryObject)
             : base(dbFactory, queryObject)
         {
-        }
-
-
-        #region get select columns
-        protected string GetHeader_Columns()
-        {
-            var header = "InvoiceHeader";
-            var columns = $@"
---{EventHelper.TableAllies}.EventUuid,
-{Helper.TableAllies}.InvoiceUuid as '{header}.InvoiceUuid',
---{Helper.TableAllies}.OrderInvoiceNum as '{header}.OrderInvoiceNum',
-{Helper.TableAllies}.DatabaseNum as '{header}.DatabaseNum',
-{Helper.TableAllies}.MasterAccountNum as '{header}.MasterAccountNum',
-{Helper.TableAllies}.ProfileNum as '{header}.ProfileNum',
-{InfoHelper.TableAllies}.ChannelNum as '{header}.ChannelNum',
-{InfoHelper.TableAllies}.ChannelAccountNum as '{header}.ChannelAccountNum',
-{Helper.TableAllies}.InvoiceNumber as '{header}.InvoiceNumber',
-{Helper.TableAllies}.InvoiceDate as '{header}.InvoiceDateUtc',
-{InfoHelper.TableAllies}.CentralOrderNum as '{header}.CentralOrderNum',
-{InfoHelper.TableAllies}.ChannelOrderID as '{header}.ChannelOrderID',
---{InfoHelper.TableAllies}.OrderDCAssignmentNum as '{header}.OrderDCAssignmentNum',
-{InfoHelper.TableAllies}.OrderShipmentNum as '{header}.OrderShipmentNum',--need join
---{InfoHelper.TableAllies}.ShipmentID as '{header}.ShipmentID',--need join
-{Helper.TableAllies}.ShipDate as '{header}.ShipmentDateUtc',
-{InfoHelper.TableAllies}.ShippingCarrier as '{header}.ShippingCarrier',
-{InfoHelper.TableAllies}.ShippingClass as '{header}.ShippingClass',
-{Helper.TableAllies}.ShippingAmount as '{header}.ShippingCost',
---{InfoHelper.TableAllies}.MainTrackingNumber as '{header}.MainTrackingNumber',--need join
-{Helper.TableAllies}.SubTotalAmount as '{header}.InvoiceAmount',
-{Helper.TableAllies}.TaxAmount as '{header}.InvoiceTaxAmount',
-{Helper.TableAllies}.ChargeAndAllowanceAmount as '{header}.InvoiceHandlingFee',
-{Helper.TableAllies}.DiscountAmount as '{header}.InvoiceDiscountAmount',
-{Helper.TableAllies}.TotalAmount as '{header}.TotalAmount',
---{Helper.TableAllies}.InvoiceTermsType as '{header}.InvoiceTermsType',
-{Helper.TableAllies}.Terms as '{header}.InvoiceTermsDescrption',
-{Helper.TableAllies}.TermsDays as '{header}.InvoiceTermsDays',
---{Helper.TableAllies}.DBChannelOrderHeaderRowID as '{header}.DBChannelOrderHeaderRowID',
-{Helper.TableAllies}.EnterDateUtc as '{header}.EnterDateUtc' 
-";
-            return columns;
-        }
-        protected string GetItem_Columns()
-        {
-            //TODO adjust column mapping.
-            var columns = $@"
-{ItemHelper.TableAllies}.Seq AS OrderInvoiceLineNum,
---{ItemHelper.TableAllies}.DatabaseNum AS DatabaseNum,
---{ItemHelper.TableAllies}.MasterAccountNum AS MasterAccountNum,
---{ItemHelper.TableAllies}.ProfileNum AS ProfileNum,
---{ItemHelper.TableAllies}.ChannelNum AS ChannelNum,
---{ItemHelper.TableAllies}.ChannelAccountNum AS ChannelAccountNum,
---{ItemHelper.TableAllies}.OrderShipmentItemNum AS OrderShipmentItemNum,
---{ItemHelper.TableAllies}.CentralOrderLineNum AS CentralOrderLineNum,
---{ItemHelper.TableAllies}.OrderDCAssignmentLineNum AS OrderDCAssignmentLineNum,
-{ItemHelper.TableAllies}.SKU AS SKU,
---{ItemHelper.TableAllies}.ChannelItemID AS ChannelItemID,
-{ItemHelper.TableAllies}.ShipQty AS ShippedQty,
-{ItemHelper.TableAllies}.DiscountPrice AS UnitPrice,
-{ItemHelper.TableAllies}.ExtAmount AS LineItemAmount,
-{ItemHelper.TableAllies}.TaxAmount AS LineTaxAmount,
-{ItemHelper.TableAllies}.ChargeAndAllowanceAmount AS LineHandlingFee,
-{ItemHelper.TableAllies}.DiscountAmount AS LineDiscountAmount,
-{ItemHelper.TableAllies}.ItemTotalAmount AS LineAmount,
---{ItemHelper.TableAllies}.DBChannelOrderLineRowID AS DBChannelOrderLineRowID,
-{ItemHelper.TableAllies}.InvoiceItemStatus AS ItemStatus,
-{ItemHelper.TableAllies}.EnterDateUtc AS EnterDateUtc
-";
-            return columns;
-        }
-        protected string GetItem_Script()
-        {
-            var columns = $@"
-( 
-SELECT 
-{GetItem_Columns()}
-FROM { ItemHelper.TableName} { ItemHelper.TableAllies}
-WHERE { ItemHelper.TableAllies}.InvoiceUuid = { Helper.TableAllies}.InvoiceUuid 
-FOR JSON PATH
-) AS InvoiceItems";
-            return columns;
-        }
-        #endregion
+        } 
 
         #region override methods
 
         protected override string GetSQL_select()
         {
             this.SQL_Select = $@"
-            SELECT 
-             {GetHeader_Columns()}
-            ,{GetItem_Script()} 
+SELECT 
+ins.InvoiceUuid as 'InvoiceHeader.InvoiceUuid',
+--ins.OrderInvoiceNum as 'InvoiceHeader.OrderInvoiceNum',
+ins.DatabaseNum as 'InvoiceHeader.DatabaseNum',
+ins.MasterAccountNum as 'InvoiceHeader.MasterAccountNum',
+ins.ProfileNum as 'InvoiceHeader.ProfileNum',
+insi.ChannelNum as 'InvoiceHeader.ChannelNum',
+insi.ChannelAccountNum as 'InvoiceHeader.ChannelAccountNum',
+ins.InvoiceNumber as 'InvoiceHeader.InvoiceNumber',
+ins.InvoiceDate as 'InvoiceHeader.InvoiceDateUtc',
+insi.CentralOrderNum as 'InvoiceHeader.CentralOrderNum',
+insi.ChannelOrderID as 'InvoiceHeader.ChannelOrderID',
+--insi.OrderDCAssignmentNum as 'InvoiceHeader.OrderDCAssignmentNum',
+insi.OrderShipmentNum as 'InvoiceHeader.OrderShipmentNum',--need join
+--insi.ShipmentID as 'InvoiceHeader.ShipmentID',--need join
+ins.ShipDate as 'InvoiceHeader.ShipmentDateUtc',
+insi.ShippingCarrier as 'InvoiceHeader.ShippingCarrier',
+insi.ShippingClass as 'InvoiceHeader.ShippingClass',
+ins.ShippingAmount as 'InvoiceHeader.ShippingCost',
+--insi.MainTrackingNumber as 'InvoiceHeader.MainTrackingNumber',--need join
+ins.SubTotalAmount as 'InvoiceHeader.InvoiceAmount',
+ins.TaxAmount as 'InvoiceHeader.InvoiceTaxAmount',
+ins.ChargeAndAllowanceAmount as 'InvoiceHeader.InvoiceHandlingFee',
+ins.DiscountAmount as 'InvoiceHeader.InvoiceDiscountAmount',
+ins.TotalAmount as 'InvoiceHeader.TotalAmount',
+--ins.InvoiceTermsType as 'InvoiceHeader.InvoiceTermsType',
+ins.Terms as 'InvoiceHeader.InvoiceTermsDescrption',
+ins.TermsDays as 'InvoiceHeader.InvoiceTermsDays',
+--ins.DBChannelOrderHeaderRowID as 'InvoiceHeader.DBChannelOrderHeaderRowID',
+ins.EnterDateUtc as 'InvoiceHeader.EnterDateUtc', 
+
+( 
+SELECT 
+insl.Seq AS OrderInvoiceLineNum,
+--insl.DatabaseNum AS DatabaseNum,
+--insl.MasterAccountNum AS MasterAccountNum,
+--insl.ProfileNum AS ProfileNum,
+--insl.ChannelNum AS ChannelNum,
+--insl.ChannelAccountNum AS ChannelAccountNum,
+--insl.OrderShipmentItemNum AS OrderShipmentItemNum,
+--insl.CentralOrderLineNum AS CentralOrderLineNum,
+--insl.OrderDCAssignmentLineNum AS OrderDCAssignmentLineNum,
+insl.SKU AS SKU,
+--insl.ChannelItemID AS ChannelItemID,
+insl.ShipQty AS ShippedQty,
+insl.DiscountPrice AS UnitPrice,
+insl.ExtAmount AS LineItemAmount,
+insl.TaxAmount AS LineTaxAmount,
+insl.ChargeAndAllowanceAmount AS LineHandlingFee,
+insl.DiscountAmount AS LineDiscountAmount,
+insl.ItemTotalAmount AS LineAmount,
+--insl.DBChannelOrderLineRowID AS DBChannelOrderLineRowID,
+insl.InvoiceItemStatus AS ItemStatus,
+insl.EnterDateUtc AS EnterDateUtc
+FROM InvoiceItems insl
+WHERE insl.InvoiceUuid = ins.InvoiceUuid 
+FOR JSON PATH
+) AS InvoiceItems
             ";
 
             return this.SQL_Select;
@@ -123,51 +93,17 @@ FOR JSON PATH
         protected override string GetSQL_from()
         {
             this.SQL_From = $@"
- FROM {EventHelper.TableName} {EventHelper.TableAllies}
- INNER JOIN {Helper.TableName} {Helper.TableAllies}  
-        on  {Helper.TableAllies}.MasterAccountNum={EventHelper.TableAllies}.MasterAccountNum
-        and {Helper.TableAllies}.ProfileNum={EventHelper.TableAllies}.ProfileNum
-        and {Helper.TableAllies}.InvoiceUuid={EventHelper.TableAllies}.ProcessUuid
- LEFT JOIN {InfoHelper.TableName} {InfoHelper.TableAllies} ON ({Helper.TableAllies}.InvoiceUuid = {InfoHelper.TableAllies}.InvoiceUuid)
+ FROM EventProcessERP epe
+ INNER JOIN InvoiceHeader ins  
+        on  ins.MasterAccountNum=epe.MasterAccountNum
+        and ins.ProfileNum=epe.ProfileNum
+        and ins.InvoiceUuid=epe.ProcessUuid
+ LEFT JOIN InvoiceHeaderInfo insi ON (ins.InvoiceUuid = insi.InvoiceUuid)
 ";
             return this.SQL_From;
-        }
-
-        //public override SqlParameter[] GetSqlParameters()
-        //{
-        //    var paramList = base.GetSqlParameters().ToList();
-        //    paramList.Add("@EventProcessActionStatusEnum".ToEnumParameter<InvoiceStatusEnum>());
-        //    paramList.Add("@EventProcessTypeEnum".ToEnumParameter<EventProcessTypeEnum>());
-
-        //    return paramList.ToArray();
-
-        //}
+        } 
 
         #endregion override methods
-
-        public virtual void GetUnprocessedInvoices(InvoicePayload payload)
-        {
-            if (payload == null)
-                payload = new InvoicePayload();
-
-            this.LoadRequestParameter(payload);
-            StringBuilder sb = new StringBuilder();
-            try
-            {
-                payload.InvoiceUnprocessListCount = Count();
-                payload.Success = ExcuteJson(sb);
-                if (payload.Success)
-                    payload.InvoiceUnprocessList = sb;
-            }
-            catch (Exception ex)
-            {
-                payload.Success = false;
-                payload.InvoiceUnprocessListCount = 0;
-                AddError(ex.ObjectToString());
-                payload.Messages = this.Messages;
-            }
-            payload.Messages.Add(this.Messages);
-        }
 
         public virtual async Task GetUnprocessedInvoicesAsync(InvoicePayload payload)
         {
