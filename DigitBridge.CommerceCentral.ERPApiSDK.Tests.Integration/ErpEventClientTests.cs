ef2dd6676fb5aa94ebc7425654ec4605997bf40b
junxian <junxian.cui@hurraymart.com>
2021-11-24 22:01:56 +0800

    Add resend event to api and sdk

---
+++ DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration/ErpEventClientTests.cs
@@ -0,0 +1,57 @@
+using DigitBridge.Base.Utility;
+using DigitBridge.CommerceCentral.XUnit.Common;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DigitBridge.CommerceCentral.ERPApiSDK.Tests.Integration
+{
+    public partial class ErpEventClientTests : IDisposable, IClassFixture<TestFixture<StartupTest>>
+    {
+        protected const string SkipReason = "Debug TableUniversalTests Function";
+
+        protected TestFixture<StartupTest> Fixture { get; }
+        public IConfiguration Configuration { get; }
+
+        private string _baseUrl = "http://localhost:7073/api/erpevents";
+        //private string _baseUrl = "https://digitbridge-erp-event-api-dev.azurewebsites.net/api/erpevents";
+        private string _code = "drZEGmRUVmGcitmCqyp3VZe5b4H8fSoy8rDUsEMkfG9U7UURXMtnrw==";
+
+        protected const int MasterAccountNum = 10001;
+        protected const int ProfileNum = 10001;
+
+        public ErpEventClientTests(TestFixture<StartupTest> fixture)
+        {
+            Fixture = fixture;
+            Configuration = fixture.Configuration;
+
+            InitForTest();
+        }
+        protected void InitForTest()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        [Fact()]
+        public async Task ResendAsync_Test()
+        {
+            var client = new ErpEventClient(_baseUrl, _code);
+            var eventUuid = "c2dc72e4-6e74-49c3-9ab6-eb2a951d5622";
+            var success = await client.ResendAsync(MasterAccountNum, ProfileNum, eventUuid);
+
+            Assert.True(success, client.Messages.ObjectToString());
+            Assert.True(client.Data != null && client.Data.EventUuid == eventUuid);
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
