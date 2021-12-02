f22c1ec34b7cdeb75a99c5d6be3c6f5f64a48e4b
junxian <junxian.cui@hurraymart.com>
2021-12-02 20:51:47 +0800

    Fix online queue not trigger.

---
+++ DigitBridge.CommerceCentral.ERPDatabase/sql/clear data by shipmentid.sql
@@ -0,0 +1,50 @@
+﻿
+select ProcessUuid into #tmpProcessUuid from EventProcessERP 
+where ERPEventProcessType=4  
+and ProcessUuid in 
+(
+ '113-10000000496',
+ '113-10000000497',
+ '113-10000000498',
+ '113-10000000499',
+ '113-10000000450'
+) 
+
+select SalesOrderUuid,InvoiceUuid,OrderShipmentUuid
+into #ToDeleteUuids
+from OrderShipmentHeader 
+where ShipmentID in 
+(
+ select * from #tmpProcessUuid
+)  
+
+--delete salesorder
+delete from SalesOrderHeader where SalesOrderUuid in (select  SalesOrderUuid from #ToDeleteUuids)
+delete from SalesOrderHeaderAttributes where SalesOrderUuid in (select  SalesOrderUuid from #ToDeleteUuids)
+delete from SalesOrderHeaderInfo where SalesOrderUuid in (select  SalesOrderUuid from #ToDeleteUuids)
+delete from SalesOrderItems where SalesOrderUuid in (select  SalesOrderUuid from #ToDeleteUuids)
+delete from SalesOrderItemsAttributes where SalesOrderUuid in (select  SalesOrderUuid from #ToDeleteUuids) 
+
+--delete invoice
+delete from InvoiceHeader where InvoiceUuid in (select  InvoiceUuid from #ToDeleteUuids)
+delete from InvoiceHeaderAttributes where InvoiceUuid in (select  InvoiceUuid from #ToDeleteUuids)
+delete from InvoiceHeaderInfo where InvoiceUuid in (select  InvoiceUuid from #ToDeleteUuids)
+delete from InvoiceItems where InvoiceUuid in (select  InvoiceUuid from #ToDeleteUuids)
+delete from InvoiceItemsAttributes where InvoiceUuid in (select  InvoiceUuid from #ToDeleteUuids)
+
+--delete shipment
+delete from OrderShipmentHeader where OrderShipmentUuid in (select  OrderShipmentUuid from #ToDeleteUuids)
+delete from OrderShipmentPackage where OrderShipmentUuid in (select  OrderShipmentUuid from #ToDeleteUuids)
+delete from OrderShipmentShippedItem where OrderShipmentUuid in (select  OrderShipmentUuid from #ToDeleteUuids)
+delete from OrderShipmentCanceledItem where OrderShipmentUuid in (select  OrderShipmentUuid from #ToDeleteUuids) 
+
+--delete eventprocess
+delete from EventProcessERP
+where ERPEventProcessType=4  
+and ProcessUuid in 
+(
+ select * from #tmpProcessUuid
+) 
+
+drop table #tmpProcessUuid
+drop table #ToDeleteUuids
