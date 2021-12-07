f32455091b383af6ad28d56f44cae940b9108648
Hanson Zhang <hanson.zhang_1985@hotmail.com>
2021-12-07 20:34:07 +0800

    SelectList API for system defined code

---
+++ DigitBridge.CommerceCentral.ERPMdl/selectList/systemCodes_ColorPatternCode.cs
@@ -0,0 +1,39 @@
+﻿using DigitBridge.CommerceCentral.YoPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitBridge.CommerceCentral.ERPMdl.selectList.customer
+{
+    public class systemCodes_ColorPatternCode : SelectListBase
+    {
+        public override string Name => "system_ColorPatternCode";
+
+        public systemCodes_ColorPatternCode(IDataBaseFactory dbFactory)
+            : base(dbFactory) 
+        { }
+
+        protected override string GetSQL_select()
+        {
+            this.SetFilterSqlString();
+            this.SQL_Select = $@"
+ 
+    SELECT 
+      tbl.SystemCodeName AS [value],
+      tbl.JsonFields AS [text]
+    FROM
+      [dbo].[SystemCodes] tbl
+   
+    WHERE COALESCE(tbl.SystemCodeName,'') == '{Name}' 
+        AND {this.QueryObject.GetSQL()}
+    ORDER BY tbl.SystemCodeName 
+";
+            return this.SQL_Select;
+        }
+
+        protected override void SetFilterSqlString()
+        {
+            base.SetFilterSqlString();
+        }
+    }
+}
