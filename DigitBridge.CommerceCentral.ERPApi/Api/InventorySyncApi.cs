e2d6b7d240ece9100a48459d6c02d659e0d820d8
Snowy <snowyyang@outlook.com>
2021-12-13 23:38:42 +0800

    move syncfromproductbasic api

--- DigitBridge.CommerceCentral.ERPApi/Api/InventorySyncApi.cs
+++ DigitBridge.CommerceCentral.ERPApi/Api/InventorySyncApi.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DigitBridge.Base.Common;
 using DigitBridge.Base.Utility;
 using DigitBridge.CommerceCentral.ApiCommon;
+using DigitBridge.CommerceCentral.AzureStorage;
 using DigitBridge.CommerceCentral.ERPApi.OpenApiModel;
 using DigitBridge.CommerceCentral.ERPDb;
 using DigitBridge.CommerceCentral.ERPDb.inventorySync;
@@ -49,6 +50,30 @@ namespace DigitBridge.CommerceCentral.ERPApi.Api
             payload.Messages = srv.Messages;
             return new JsonNetResponse<InventorySyncUpdatePayload>(payload);
  
+        }
+
+        [FunctionName(nameof(SyncFromProductBasic))]
+        [OpenApiOperation(operationId: "SyncFromProductBasic", tags: new[] { "ProductExts" })]
+        [OpenApiParameter(name: "masterAccountNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "MasterAccountNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
+        [OpenApiParameter(name: "profileNum", In = ParameterLocation.Header, Required = true, Type = typeof(int), Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
+        [OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "API Keys", Description = "Azure Function App key", Visibility = OpenApiVisibilityType.Advanced)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(ExistSKUPayload))]
+        public static async Task SyncFromProductBasic([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "productExts/syncProducts")] HttpRequest req)
+        {
+            var masterAccountNum = req.GetHeaderValue("masterAccountNum").ToInt();
+            var profileNum = req.GetHeaderValue("profileNum").ToInt();
+            var dbFactory = await MyAppHelper.CreateDefaultDatabaseAsync(masterAccountNum);
+            var uuids = dbFactory.Db.Query<string>(
+@$"SELECT pb.ProductUuid FROM ProductBasic pb LEFT JOIN ProductExt pe ON pb.ProductUuid=pe.ProductUuid
+WHERE pb.MasterAccountNum={masterAccountNum} AND pb.ProfileNum={profileNum} AND pe.ProductUuid IS NULL");
+            foreach (var uuid in uuids)
+                await QueueUniversal<ERPQueueMessage>.SendMessageAsync(ERPQueueSetting.ERPSyncProductQueue, MySingletonAppSetting.AzureWebJobsStorage, new ERPQueueMessage()
+                {
+                    //DatabaseNum = payload.DatabaseNum,
+                    MasterAccountNum = masterAccountNum,
+                    ProfileNum = profileNum,
+                    ProcessUuid = uuid,
+                });
         }
     }
 }
