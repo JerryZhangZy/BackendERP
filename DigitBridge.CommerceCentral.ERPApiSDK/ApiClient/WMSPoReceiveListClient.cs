4ab9e9f1f6e433f04d8cd89bcb2abefa8f2ea094
junxian <junxian.cui@hurraymart.com>
2021-12-20 20:56:40 +0800

    Add poreceive list to integration api and sdk. Add unit test for this.

---
+++ DigitBridge.CommerceCentral.ERPApiSDK/ApiClient/WMSPoReceiveListClient.cs
@@ -0,0 +1,82 @@
+﻿using DigitBridge.Base.Utility;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitBridge.CommerceCentral.ERPApiSDK
+{
+    /// <summary>
+    /// ERP providing PoReceive (which uploaded by WMS) transferred result for WMS to download.
+    /// </summary>
+    public class WMSPoReceiveListClient : ApiClientBase<WmsPoReceiveListPayload>
+    {
+        public IList<WMSPoReceiveProcess> Data { get; set; }
+        /// <summary>
+        /// "ERP_Integration_Api_BaseUrl" and "ERP_Integration_Api_AuthCode" were not config in config file
+        /// Local config file is 'local.settings.json'
+        /// </summary>
+        public WMSPoReceiveListClient() : base(ConfigUtil.ERP_Integration_Api_BaseUrl, ConfigUtil.ERP_Integration_Api_AuthCode)
+        {
+
+        }
+        /// <summary>
+        /// "ERP_Integration_Api_BaseUrl" and "ERP_Integration_Api_AuthCode" were config in config file
+        /// Local config file is 'local.settings.json'
+        /// </summary>
+        /// <param name="baseUrl"></param>
+        /// <param name="authCode"></param>
+        public WMSPoReceiveListClient(string baseUrl, string authCode) : base(baseUrl, authCode)
+        { }
+
+
+        /// <summary>
+        /// Get wms PoReceive list by array of PoReceiveid
+        /// </summary>
+        /// <param name="masterAccountNum"></param>
+        /// <param name="profileNum"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        public async Task<bool> GetWMSOrderPoReceiveListAsync(int masterAccountNum, int profileNum, IList<string> wmsBatchNums)
+        {
+            if (wmsBatchNums is null || wmsBatchNums.Count == 0)
+            {
+                AddError("wmsBatchNums cann't be empty.");
+                return false;
+            }
+
+            if (!SetAccount(masterAccountNum, profileNum))
+            {
+                return false;
+            }
+
+            return await PostAsync(wmsBatchNums, FunctionUrl.WMSPoReceiveList);
+        }
+
+        protected override async Task<bool> AnalysisResponseAsync(string responseData)
+        {
+            if (ResopneData == null)
+            {
+                AddError(responseData);
+
+                //Maybe the api throw exception.
+                //var exception = JsonConvert.DeserializeObject<Exception>(responseData, jsonSerializerSettings);
+                //if (exception != null)
+                //    AddError(exception.ObjectToString());
+                return false;
+            }
+
+            if (!ResopneData.Success)
+            {
+                this.Messages.Add(ResopneData.Messages);
+            }
+            else
+            {
+                Data = ResopneData.WMSPoReceiveProcessesList;
+            }
+            return ResopneData.Success;
+        }
+    }
+}
--- DigitBridge.CommerceCentral.ERPApiSDK/Model/Payload/WMSPoReceivePayload.cs
+++ DigitBridge.CommerceCentral.ERPApiSDK/Model/Payload/WMSPoReceivePayload.cs
@@ -53,4 +53,54 @@ namespace DigitBridge.CommerceCentral.ERPApiSDK
         /// </summary>
         public string TransUuid { get; set; }
     }
+
+    #region Get wms PoReceive list 
+
+    public class WmsPoReceiveListPayload : ResponsePayloadBase
+    {
+        public IList<WMSPoReceiveProcess> WMSPoReceiveProcessesList;
+    }
+    public class WMSPoReceiveProcess
+    {
+        /// <summary>
+        /// The time of po transation tranferred from PoReceive.
+        /// </summary>
+        public DateTime? ProcessDate { get; set; }
+
+        /// <summary>
+        /// The unique number of the request wms batch num.
+        /// </summary>
+        public string WMSBatchNum { get; set; }
+
+        /// <summary>
+        /// Process status 
+        /// </summary>
+        public int ProcessStatus { get; set; }
+
+        /// <summary>
+        /// Process status  description
+        /// </summary>
+        public string ProcessStatusText { get; set; }
+
+        /// <summary>
+        /// Error message
+        /// </summary>
+        public string EventMessage { get; set; }
+
+        /// <summary>
+        /// LastUpdateDate
+        /// </summary>
+        public DateTime LastUpdateDate { get; set; }
+        /// <summary>
+        /// UpdateDateUtc
+        /// </summary>
+        public DateTime UpdateDateUtc { get; set; }
+        /// <summary>
+        /// EnterDateUtc: the date wms upload PoReceive.
+        /// </summary>
+        public DateTime EnterDateUtc { get; set; }
+        public string EnterBy { get; set; }
+        public string UpdateBy { get; set; }
+    }
+    #endregion
 }
