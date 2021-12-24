09b59d9328a4dbde14850c5a9bb854b3e398dc43
Snowy <snowyyang@outlook.com>
2021-12-25 04:27:51 +0800

    profilenum index

--- DigitBridge.CommerceCentral.ERPDatabase/inventory/Inventory_Update.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/inventory/Inventory_Update.sql
@@ -27,3 +27,12 @@ CREATE NONCLUSTERED INDEX [IX_Inventory_ProductUuid] ON [dbo].[Inventory]
 	[ProductUuid] ASC
 ) 
 GO
+
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Inventory]') AND name = N'IX_Inventory_PN_S')
+CREATE NONCLUSTERED INDEX [IX_Inventory_PN_S] ON [dbo].[Inventory]
+(
+	[ProfileNum] ASC,
+	[SKU] ASC
+) 
+GO
