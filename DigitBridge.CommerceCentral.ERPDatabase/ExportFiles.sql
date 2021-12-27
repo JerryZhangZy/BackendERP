25e4bbb3a75445cd0e28b9b10b5c87119a2e7eb9
Snowy <snowyyang@outlook.com>
2021-12-28 00:43:47 +0800

    exportfiles sql

---
+++ DigitBridge.CommerceCentral.ERPDatabase/ExportFiles.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[ExportFiles](
+	[RowNum] [bigint] IDENTITY(1,1) NOT NULL,
+	[DatabaseNum] [int] NOT NULL,
+	[MasterAccountNum] [int] NOT NULL,
+	[ProfileNum] [int] NOT NULL,
+	[Status] [tinyint] NOT NULL DEFAULT 0,
+	[Category] [varchar](50) NOT NULL,
+	[ContainerReference] [varchar](255) NOT NULL,
+	[BlobReference] [varchar](255) NOT NULL,
+	[UpdateDateUtc] [datetime] NULL,
+	[EnterDateUtc] [datetime] NOT NULL DEFAULT (getutcdate()),
+ CONSTRAINT [PK_ExportFiles] PRIMARY KEY CLUSTERED 
+(
+	[RowNum] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+--IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ExportFiles]') AND name = N'IX_ExportFiles_AuthStatus')
+CREATE NONCLUSTERED INDEX [IX_ExportFiles_AuthStatus] ON [dbo].[ExportFiles]
+(
+	[MasterAccountNum] ASC,
+	[ProfileNum] ASC,
+	[Status] ASC
+) 
+GO
