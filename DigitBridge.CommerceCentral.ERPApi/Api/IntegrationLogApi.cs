32820abfb2ac850b20fb23cd5b9066113f501e8d
Snowy <snowyyang@outlook.com>
2021-12-30 10:30:42 +0800

    activityloglist swagger

--- DigitBridge.CommerceCentral.ERPApi/Api/IntegrationLogApi.cs
+++ DigitBridge.CommerceCentral.ERPApi/Api/IntegrationLogApi.cs
@@ -68,7 +68,7 @@ namespace DigitBridge.CommerceCentral.ERPApi
             Summary = "ProfileNum", Description = "From login profile", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string),
             Summary = "API Keys", Description = "Azure Function App key", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(EventERPPayloadFind),
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(ActivityLogPayloadFind),
             Description = "Request Body in json format")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json",
             bodyType: typeof(ActivityLogPayloadFind))]
