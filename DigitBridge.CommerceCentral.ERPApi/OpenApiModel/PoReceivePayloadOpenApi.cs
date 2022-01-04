1878e874ffa6f383023c71c44ad5c6c12545f695
leo <270716404@qq.com>
2022-01-04 11:05:09 +0800

    add sample data VendorCode

--- DigitBridge.CommerceCentral.ERPApi/OpenApiModel/PoReceivePayloadOpenApi.cs
+++ DigitBridge.CommerceCentral.ERPApi/OpenApiModel/PoReceivePayloadOpenApi.cs
@@ -145,6 +145,7 @@ namespace DigitBridge.CommerceCentral.ERPApi
         public long  TransNum { get; set; }
         public string PoUuid { get; set; }
         public string VendorUuid { get; set; }
+        public string VendorCode { get; set; }
         public string VendorName { get; set; }
         public string VendorInvoiceNumFrom { get; set; }
         public string VendorInvoiceNumTo { get; set; }
@@ -167,6 +168,7 @@ namespace DigitBridge.CommerceCentral.ERPApi
                     .RuleFor(u => u.TransNum, f => 0)
                     .RuleFor(u => u.PoUuid, f => string.Empty)
                     .RuleFor(u => u.VendorName, f => string.Empty)
+                     .RuleFor(u => u.VendorCode, f => string.Empty)
                     .RuleFor(u => u.PoNumFrom, f => string.Empty)
                     .RuleFor(u => u.PoNumTo, f => string.Empty)
                     .RuleFor(u => u.TransDateFrom, f => f.Date.Past(0).Date.Date.AddDays(-30))
