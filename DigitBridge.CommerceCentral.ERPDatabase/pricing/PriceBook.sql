ae2c0b8e7ec13083edbb058a49c190f6b8997fb6
Jerry Zhang <Jerry.Zhang@DigitBridge.com>
2022-01-06 21:05:40 -0800

    Fix systemCode list bugs

--- DigitBridge.CommerceCentral.ERPDatabase/pricing/PriceBook.sql
+++ DigitBridge.CommerceCentral.ERPDatabase/pricing/PriceBook.sql
@@ -8,14 +8,12 @@
     [PriceBookUuid] VARCHAR(50) NOT NULL DEFAULT '', --(Readonly) Order Item Line uuid. <br> Display: false, Editable: false
 	[PriceCode] Varchar(50) NOT NULL DEFAULT '', --VariationSet Code. <br> Title: Variation Set, Display: true, Editable: true
 
-	[Description] NVarchar(200) NOT NULL DEFAULT '', --Item line description, default from ProductBasic data. <br> Title: Description, Display: true, Editable: true
-	[Inactive] TINYINT NOT NULL DEFAULT 0, --item will update inventory instock qty. <br> Title: Stockable, Display: true, Editable: true
-	[StartDate] DATE NULL, --start active date. <br> Title: Start Date, Display: true, Editable: true
-	[EndDate] DATE NULL, --start active date. <br> Title: End Date, Display: true, Editable: true
-
 	[SKU] Varchar(100) NOT NULL DEFAULT '', --Product SKU. <br> Title: SKU, Display: true, Editable: true
 	[ProductUuid] VARCHAR(50) NOT NULL, --(Readonly) Product uuid. load from ProductBasic data. <br> Display: false, Editable: false
 	[InventoryUuid] VARCHAR(50) NOT NULL, --(Readonly) Inventory Item Line uuid, load from inventory data. <br> Display: false, Editable: false
+
+	[BusinessType] VARCHAR(50) NOT NULL DEFAULT '', --Customer business type. <br> Title: Business Type, Display: true, Editable: true
+	[PriceType] VARCHAR(50) NOT NULL DEFAULT '', --Customer price type. <br> Title: Price Type, Display: true, Editable: true
 
     [CustomerUuid] VARCHAR(50) NOT NULL DEFAULT (CAST(newid() AS NVARCHAR(50))), --Customer uuid. <br> Display: false, Editable: false.
 	[CustomerCode] VARCHAR(50) NOT NULL DEFAULT '', --Readable customer number, unique in same database and profile. <br> Parameter should pass ProfileNum-CustomerCode. <br> Title: Customer Number, Display: true, Editable: true
@@ -27,6 +25,8 @@
 	[DiscountAmount] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Item level discount amount. <br> Title: Discount Amount, Display: true, Editable: true
 	[MarkdownRate] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Item level discount rate. <br> Title: Discount Rate, Display: true, Editable: true
 	[MarkupRate] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Item level discount rate. <br> Title: Discount Rate, Display: true, Editable: true
+	[FromQty] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Qty price . <br> Title: Unit Price, Display: true, Editable: true
+	[ToQty] DECIMAL(24, 6) NOT NULL DEFAULT 0, --Item unit price. <br> Title: Unit Price, Display: true, Editable: true
 
     [UpdateDateUtc] DATETIME NULL, --(Ignore) 
     [EnterBy] Varchar(100) NOT NULL DEFAULT '', --(Ignore) 
